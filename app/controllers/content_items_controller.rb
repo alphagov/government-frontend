86c02b116f7fb412a7182c3f9b55207cf928922b
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-11-17 15:36:25 +0000

    Documents controller, route, layout and view.
    Basic functionality to get an item to the content store and display it
    on a page. Views are very basic at this point, and controller just
    passes the content hash directly to the page.

---
+++ app/controllers/content_items_controller.rb
@@ -0,0 +1,22 @@
+require 'gds_api/content_store'
+
+class ContentItemsController < ApplicationController
+
+  def show
+    if @content_item = content_store.content_item(content_item_path)
+      set_expiry(@content_item.expires_in)
+    else
+      render text: 'Not found', status: :not_found
+    end
+  end
+
+  private
+
+  def content_item_path
+    '/' + params[:path]
+  end
+
+  def content_store
+    @content_store ||= GdsApi::ContentStore.new(Plek.current.find("content-store"))
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'healthcheck', to: proc { [200, {}, ['']] }
+  get '*path' => 'content_items#show'
 end
