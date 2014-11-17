6bb59af5337eaa42e8047c8d208952efcd5c6eee
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-11-17 14:45:26 +0000

    Base controller methods and cache config.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module GovernmentFrontend
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Disable rack::cache
+    config.action_dispatch.rack_cache = nil
+
+    # Caching
+    config.cache_control_directive = 'public'
   end
 end
