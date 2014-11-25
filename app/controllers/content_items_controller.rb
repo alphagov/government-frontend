dfc406832326c88bb3100eea3571901934491bd3
Edd Sowden <e@e26.co.uk>
2014-11-25 10:43:53 +0000

    Create presenter for content_item
    This will need to be split at some point in the not to distant future
    into a case study presenter and a generic edition like presenter. But
    for the moment it is in a generic content item presenter.
    
    Also use components to make the page closer represent the direction it
    is heading in.

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -3,8 +3,10 @@ require 'gds_api/content_store'
 class ContentItemsController < ApplicationController
 
   def show
-    if @content_item = content_store.content_item(content_item_path)
-      set_expiry(@content_item.expires_in)
+    if content_item = content_store.content_item(content_item_path)
+      set_expiry(content_item.expires_in)
+
+      @content_item = ContentItemPresenter.new(content_item)
     else
       render text: 'Not found', status: :not_found
     end
---
+++ app/presenters/content_item_presenter.rb
@@ -0,0 +1,72 @@
+class ContentItemPresenter
+  include ActionView::Helpers::UrlHelper
+
+  attr_reader :content_item, :title, :description, :body, :format, :context
+
+  def initialize(content_item)
+    @content_item = content_item
+
+    @title = content_item["title"]
+    @description = content_item["description"]
+    @body = content_item["details"]["body"]
+    @format = content_item["format"]
+    @context = "Case study" #content_itme["details"]["type"]
+  end
+
+  def from
+    links("lead_organisations") + links("supporting_organisations")
+  end
+
+  def part_of
+    []
+  end
+
+  def history
+    return [] unless any_updates?
+    @content_item["details"]["change_history"].map do |item|
+      {
+        display_time: display_time(item["public_timestamp"]),
+        note: item["note"],
+        timestamp: item["public_timestamp"]
+      }
+    end
+  end
+
+  def published
+    display_time(@content_item["details"]["first_public_at"])
+  end
+
+  def updated
+    if any_updates?
+      display_time(@content_item["public_updated_at"])
+    end
+  end
+
+  def short_history
+    if any_updates?
+      "Updated #{updated_at}"
+    else
+      "Published #{published}"
+    end
+  end
+
+  def image
+    @content_item["details"]["image"]
+  end
+
+private
+  def display_time(timestamp)
+    Date.parse(timestamp).strftime("%-d %B %Y") if timestamp
+  end
+
+  def any_updates?
+    Date.parse(@content_item["public_updated_at"]) != Date.parse(@content_item["details"]["first_public_at"])
+  end
+
+  def links(type)
+    return [] unless @content_item["links"][type]
+    @content_item["links"][type].map do |link|
+      link_to(link["title"], link["base_path"])
+    end
+  end
+end
