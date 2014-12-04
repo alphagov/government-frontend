a9aaccb363094ec9a2aa5218647562b06f0f5ec3
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-04 12:20:47 +0000

    Use accessors for better encapsulation

--- app/presenters/content_item_presenter.rb
+++ app/presenters/content_item_presenter.rb
@@ -24,7 +24,7 @@ class ContentItemPresenter
 
   def history
     return [] unless any_updates?
-    @content_item["details"]["change_history"].map do |item|
+    content_item["details"]["change_history"].map do |item|
       {
         display_time: display_time(item["public_timestamp"]),
         note: item["note"],
@@ -34,12 +34,12 @@ class ContentItemPresenter
   end
 
   def published
-    display_time(@content_item["details"]["first_public_at"])
+    display_time(content_item["details"]["first_public_at"])
   end
 
   def updated
     if any_updates?
-      display_time(@content_item["public_updated_at"])
+      display_time(content_item["public_updated_at"])
     end
   end
 
@@ -52,11 +52,11 @@ class ContentItemPresenter
   end
 
   def image
-    @content_item["details"]["image"]
+    content_item["details"]["image"]
   end
 
   def text_direction
-    I18n.t("i18n.direction", locale: @locale.to_sym, default: "ltr")
+    I18n.t("i18n.direction", locale: locale.to_sym, default: "ltr")
   end
 
 private
@@ -66,12 +66,12 @@ private
   end
 
   def any_updates?
-    Date.parse(@content_item["public_updated_at"]) != Date.parse(@content_item["details"]["first_public_at"])
+    Date.parse(content_item["public_updated_at"]) != Date.parse(content_item["details"]["first_public_at"])
   end
 
   def links(type)
-    return [] unless @content_item["links"][type]
-    @content_item["links"][type].map do |link|
+    return [] unless content_item["links"][type]
+    content_item["links"][type].map do |link|
       link_to(link["title"], link["base_path"])
     end
   end
