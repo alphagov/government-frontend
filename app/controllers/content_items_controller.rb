55a13d4b24a61ce82843b046b234e19756545b92
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-09 17:34:35 +0000

    Set locale based on the content item
    When displaying a translated content item, we want the locale to be set
    accordingly. This ensures that we get the appropriate translated text in the
    view template.

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,18 +1,26 @@
 require 'gds_api/content_store'
 
 class ContentItemsController < ApplicationController
+  before_action :load_content_item
+  before_action :set_locale
 
   def show
+  end
+
+private
+
+  def load_content_item
     if content_item = content_store.content_item(content_item_path)
       set_expiry(content_item.expires_in)
-
       @content_item = ContentItemPresenter.new(content_item)
     else
       render text: 'Not found', status: :not_found
     end
   end
 
-  private
+  def set_locale
+    I18n.locale = @content_item.locale || I18n.default_locale
+  end
 
   def content_item_path
     '/' + URI.encode(params[:path])
