87e7faf81beac12623fb1ea3ca09aa45bd7b48b8
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-03-04 16:14:57 +0000

    Add government navigation
    Government pages need the government navigation which appears in the banner.
    
    I had to update Slimmer to get the improved test behaviour of components
    which allows the test to check that the component had been called and had been
    called with the correct parameters.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def wrapper_class
     "direction-#{page_text_direction}" if page_text_direction
   end
+
+  def active_proposition
+    # Which of the government sections is the page part of?
+    # Derive this from the request path, eg: /government/consultations => consultations
+    request.original_fullpath.split('/')[2]
+  end
 end
