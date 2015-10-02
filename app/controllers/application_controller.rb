b0dad9d75bb4c6f0f17b0f4f7c3e4b1a1e8cabaf
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2015-10-02 08:03:36 +0000

    Upgrade Slimmer, stop explicitly setting 'core_layout'
    Slimmer 9.0.0 switched to `core_layout` as it's default layout option,
    as it's the modern/recommend option. Apps explicitly setting a
    layout should be outliers we plan on fixing.
    
    Not setting the layout if we don't need to will make those outliers
    easier to identify and simplifies the app controller code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 class ApplicationController < ActionController::Base
-  include Slimmer::Headers
   include Slimmer::SharedTemplates
-  before_filter :set_slimmer_headers
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-private
-
-  def set_slimmer_headers
-    response.headers[Slimmer::Headers::TEMPLATE_HEADER] = "core_layout"
-  end
 end
