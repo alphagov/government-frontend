0e61d1563cd1dfaf3ca434c6c22cdef807a42dbf
Tadas Tamosauskas <tadas@pdfcv.com>
2015-10-26 10:28:17 +0000

    Show updated_at fields for service manual guides
    Replace the placeholder value with an actual value from content-api. It
    displays a human readable 'time ago' label and adds a title attribute to show
    the actual time stamp on hover. The link is planned to reference a diff in
    future, but it's not available yet. I'm leaving it as a link for styling
    purposes while we work on it.
    
    This commit also makes use of govuk content schema helpers to load an example
    API response as a text fixture.

--- app/presenters/service_manual_guide_presenter.rb
+++ app/presenters/service_manual_guide_presenter.rb
@@ -1,4 +1,5 @@
 class ServiceManualGuidePresenter
+  include ActionView::Helpers::DateHelper
   attr_reader :content_item, :title, :body, :format, :locale, :publish_time, :header_links
 
   def initialize(content_item)
@@ -9,5 +10,19 @@ class ServiceManualGuidePresenter
     @header_links = content_item["details"]["header_links"]
                       .map{|h| ActiveSupport::HashWithIndifferentAccess.new(h)}
     @format = content_item["format"]
+  end
+
+  def last_updated_ago_in_words
+    "#{time_ago_in_words(updated_at)} ago"
+  end
+
+  def last_update_timestamp
+    updated_at.strftime("%e %B %Y %H:%M")
+  end
+
+private
+
+  def updated_at
+    DateTime.parse(content_item["public_updated_at"])
   end
 end
