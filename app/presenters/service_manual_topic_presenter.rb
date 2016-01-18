9e428af018ed4e56c67de6a6c0e49d37d1efa80e
Tadas Tamosauskas <tadas@pdfcv.com>
2016-01-18 11:52:49 +0000

    Change presenter logic to use the 'topic' data format
    it was decided to use the existing 'topic' format to represent service manual
    topic pages, therefore the previously written code needed to be changed to use
    this format. I also renames some methods and private classes, also moves them
    to their own name-spaced files.
    
    The code has been made more robust to deal with the fact that we allow
    unpublished content items to be referenced in topic groups. If the content item
    is not published, it will be removed from the "links -> linked items" hash and
    the code will simply omit the referenced item. If all the items in a group are
    omitted, then the whole group will be ignored too.

--- app/presenters/service_manual_topic_presenter.rb
+++ app/presenters/service_manual_topic_presenter.rb
@@ -10,52 +10,10 @@ class ServiceManualTopicPresenter
     @locale = content_item["locale"]
   end
 
-  def link_groups
-    links_data = content_item['links']['linked_items']
+  def groups
+    linked_items = content_item['links']['linked_items']
     Array(content_item['details']['groups']).map do |group_data|
-      LinkGroup.new(group_data, links_data)
-    end
+      ServiceManualTopicPresenter::TopicGroup.new(group_data, linked_items)
+    end.select(&:present?)
   end
-
-private
-
-  class LinkGroup
-    attr_reader :name, :description, :data
-
-    def initialize(data, links)
-      @data = data
-      @links = links
-
-      @name = data['name']
-      @description = data['description']
-    end
-
-    def linked_items
-      Array(data['content_ids']).map do |content_id|
-        LinkedItem.new(content_id, @links)
-      end
-    end
-  end
-
-  class LinkedItem
-    def initialize(content_id, links_data)
-      @content_id = content_id
-      @links_data = links_data
-    end
-
-    def label
-      link_data['title']
-    end
-
-    def href
-      link_data['base_path']
-    end
-
-  private
-
-    def link_data
-      @link_data ||= @links_data.find { |ld| ld['content_id'] == @content_id}
-    end
-  end
-
 end
