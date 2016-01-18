61b400fce8ebcc2afb850ad3f1fd2bda68c54524
Tadas Tamosauskas <tadas@pdfcv.com>
2016-01-18 11:52:49 +0000

    Change presenter logic to use the 'topic' data format
    it was decided to use the existing 'topic' format to represent service manual
    topic pages, therefore the previously written code needed to be changed to use
    this format. I also renames some methods and private classes, also moves them
    to their own name-spaced files.
    
    The code has been made more robust to deal with the fact that we allow
    unpublished content items to be referenced in topic groups. If the content item
    is not published, it will be removed from the "links -> linked items" hash and
    the code will simply omit the referenced item. If all the items in a group are
    omitted, then the whole group will be ignored too.

--- app/presenters/topic_presenter.rb
+++ app/presenters/topic_presenter.rb
@@ -10,52 +10,11 @@ class TopicPresenter
     @locale = content_item["locale"]
   end
 
-  def link_groups
-    links_data = content_item['links']['linked_items']
-    Array(content_item['details']['groups']).map do |group_data|
-      LinkGroup.new(group_data, links_data)
+  def groups
+    linked_items = content_item['links']['linked_items']
+    topic_groups = Array(content_item['details']['groups']).map do |group_data|
+      TopicPresenter::TopicGroup.new(group_data, linked_items)
     end
+    topic_groups.select(&:present?)
   end
-
-private
-
-  class LinkGroup
-    attr_reader :name, :description, :data
-
-    def initialize(data, links)
-      @data = data
-      @links = links
-
-      @name = data['name']
-      @description = data['description']
-    end
-
-    def linked_items
-      Array(data['content_ids']).map do |content_id|
-        LinkedItem.new(content_id, @links)
-      end
-    end
-  end
-
-  class LinkedItem
-    def initialize(content_id, links_data)
-      @content_id = content_id
-      @links_data = links_data
-    end
-
-    def label
-      link_data['title']
-    end
-
-    def href
-      link_data['base_path']
-    end
-
-  private
-
-    def link_data
-      @link_data ||= @links_data.find { |ld| ld['content_id'] == @content_id}
-    end
-  end
-
 end
---
+++ app/presenters/topic_presenter/topic_group.rb
@@ -0,0 +1,22 @@
+class TopicPresenter::TopicGroup
+  attr_reader :name, :description, :data
+
+  def initialize(data, linked_items)
+    @data = data
+    @linked_items = linked_items
+
+    @name = data['name']
+    @description = data['description']
+  end
+
+  def linked_items
+    linked_items = Array(data['content_ids']).map do |content_id|
+      TopicPresenter::LinkedItem.new(content_id, @linked_items)
+    end
+    linked_items.select(&:present?)
+  end
+
+  def present?
+    linked_items.any?
+  end
+end
