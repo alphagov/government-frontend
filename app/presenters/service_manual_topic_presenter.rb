2c0c8dcb91df9cbec0fbd2cb4ed0890939b101e4
Tadas Tamosauskas <tadas@pdfcv.com>
2016-01-26 16:15:43 +0000

    Display real data about content owners
    Enrich hardcoded text in the sidebar with real data and links from the content
    store payload.
    
    Make the text more generic as it the old copy did not work well
    with a variable number of links.
    
    Copy author Louisa Harper.

--- app/presenters/service_manual_topic_presenter.rb
+++ app/presenters/service_manual_topic_presenter.rb
@@ -1,4 +1,5 @@
 class ServiceManualTopicPresenter
+  ContentOwner = Struct.new(:title, :href)
   attr_reader :content_item, :title, :format, :description, :locale
 
   def initialize(content_item)
@@ -15,5 +16,11 @@ class ServiceManualTopicPresenter
     Array(content_item['details']['groups']).map do |group_data|
       ServiceManualTopicPresenter::TopicGroup.new(group_data, linked_items)
     end.select(&:present?)
+  end
+
+  def content_owners
+    @content_owners ||= Array(content_item['links']['content_owners']).map do |data|
+      ContentOwner.new(data['title'], data['base_path'])
+    end
   end
 end
