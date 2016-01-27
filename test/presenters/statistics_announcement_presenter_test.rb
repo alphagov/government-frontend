35679644b3621e035a06230d5312095d2b02aaa6
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-01-27 18:20:53 +0000

    Refactor stats announcement presenter tests
    Use content schema examples, rather than stubbing fields explicitly
    in the test.
    
    This more closely couples the presenter and the schemas repo and
    changes to the later are more likely to cause test failures with
    the former. But this is probably a good thing, as we're treating
    schema changes as the authority in our system, if they or the
    examples change we should have to update apps.
    
    Also, this approach enforces that schema examples cover at least
    enough cases to support testing, making it less likely to have
    situations where a schema only has an example for the "happy case".
    This also has the happy side-effect of ensuring frontends will
    have more examples for manual testing against

--- test/presenters/statistics_announcement_presenter_test.rb
+++ test/presenters/statistics_announcement_presenter_test.rb
@@ -5,123 +5,75 @@ class StatisticsAnnouncementPresenterTest < ActiveSupport::TestCase
   include ActionView::Helpers::UrlHelper
 
   test 'presents from as links to organisations' do
-    item = presented_statistics_announcement({
-      "links" => {
-        "organisations" => [{ "title" => 'Org', "base_path" => '/orgs/org'}]
-      }
-    })
     links = [
-      link_to('Org', '/orgs/org')
+      link_to('NHS England', '/government/organisations/nhs-commissioning-board')
     ]
-    assert_equal links, item.from
+    assert_equal links, statistics_announcement.from
   end
 
   test 'presents part_of as links to a topic' do
-    item = presented_statistics_announcement({
-      "links" => {
-        "policy_areas" => [{ "title" => 'Policy Area', "base_path" => '/orgs/policy-area'}]
-      }
-    })
     links = [
-      link_to('Policy Area', '/orgs/policy-area')
+      link_to('National Health Service', '/government/topics/national-health-service')
     ]
-    assert_equal links, item.part_of
+    assert_equal links, statistics_announcement.part_of
   end
 
   test 'presents release_date' do
-    item = presented_statistics_announcement({
-      "details" => {
-        "display_date" => "April to May 2016"
-      }
-    })
-    assert_equal 'April to May 2016', item.release_date
+    assert_equal '20 January 2016 9:30am', statistics_announcement.release_date
   end
 
   test 'presents release_date_and_status when confirmed' do
-    item = presented_statistics_announcement({
-      "details" => {
-        "display_date" => "April to May 2016",
-        "state" => "confirmed"
-      }
-    })
-    assert_equal 'April to May 2016 (confirmed)', item.release_date_and_status
+    assert_equal '20 January 2016 9:30am (confirmed)', statistics_announcement.release_date_and_status
   end
 
   test 'presents release_date_and_status when provisional' do
-    item = presented_statistics_announcement({
-      "details" => {
-        "display_date" => "April to May 2016",
-        "state" => "provisional"
-      }
-    })
-    assert_equal 'April to May 2016 (provisional)', item.release_date_and_status
+    assert_equal 'January 2016 (provisional)', statistics_announcement_provisional.release_date_and_status
   end
 
   test 'presents release_date_and_status when cancelled' do
-    item = presented_cancelled_statistics_announcement
-    assert_equal 'April to May 2016', item.release_date_and_status
+    assert_equal '20 January 2016 9:30am', statistics_announcement_cancelled.release_date_and_status
   end
 
   test "present other metadata when confirmed" do
-    item = presented_statistics_announcement({
-      "details" => {
-        "display_date" => "April to May 2016",
-        "state" => "confirmed"
-      }
-    })
     other = {
-      "Release date" => "April to May 2016 (confirmed)"
+      "Release date" => "20 January 2016 9:30am (confirmed)"
     }
-    assert_equal other, item.other_metadata
+    assert_equal other, statistics_announcement.other_metadata
   end
 
   test "present other metadata when cancelled" do
-    item = presented_cancelled_statistics_announcement
     other = {
-      "Proposed release" => "April to May 2016",
+      "Proposed release" => "20 January 2016 9:30am",
       "Cancellation date" => "17 January 2016 2:19pm"
     }
-    assert_equal other, item.other_metadata
+    assert_equal other, statistics_announcement_cancelled.other_metadata
   end
 
   test "shows the cancellation reason when cancelled" do
-    item = presented_cancelled_statistics_announcement
-
-    assert_equal "Release cancelled for operational reasons.", item.cancellation_reason
+    item = statistics_announcement_cancelled
+    assert_equal "Official Statistics", item.cancellation_reason
   end
 
   test 'knows if an item is a national statistic' do
-    item = presented_statistics_announcement({
-      "details" => {
-        "format_sub_type" => "official"
-      }
-    })
-    assert_not item.national_statistics?
-
-    item = presented_statistics_announcement({
-      "details" => {
-        "format_sub_type" => "national"
-      }
-    })
-    assert item.national_statistics?
+    assert_not statistics_announcement.national_statistics?
+    assert statistics_announcement_national.national_statistics?
   end
 
-  def presented_statistics_announcement(overrides = {})
-    StatisticsAnnouncementPresenter.new(statistics_announcement.merge(overrides))
+  def statistics_announcement_cancelled
+    statistics_announcement('cancelled_official_statistics')
   end
 
-  def presented_cancelled_statistics_announcement(overrides = {})
-    presented_statistics_announcement({
-      "details" => {
-        "display_date" => "April to May 2016",
-        "state" => "cancelled",
-        "cancelled_at" => "2016-01-17T14:19:42.460Z",
-        "cancellation_reason" => "Release cancelled for operational reasons."
-      }
-    })
+  def statistics_announcement_provisional
+    statistics_announcement('national_statistics')
   end
 
-  def statistics_announcement
-    govuk_content_schema_example('statistics_announcement', 'official_statistics')
+  def statistics_announcement_national
+    statistics_announcement('national_statistics')
   end
+
+  def statistics_announcement(type='official_statistics')
+    content_item = govuk_content_schema_example('statistics_announcement', type)
+    StatisticsAnnouncementPresenter.new(content_item)
+  end
+
 end
