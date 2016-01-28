5cbac53b9cd41425550aae4ebc81eb6915697774
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-01-28 16:43:58 +0000

    Add Case Study integration tests
    Not exhuasutive, but enough to illustrate the pattern of a test
    per example, and ensure there's nothing dependant on Stats
    Announcement or Take Part

---
+++ test/integration/case_study_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class CaseStudyTest < ActionDispatch::IntegrationTest
+  test "translated case study" do
+    setup_and_visit_content_item('translated')
+
+    assert_has_component_title(@content_item["title"])
+    assert page.has_text?(@content_item["description"])
+
+    assert page.has_css?('.available-languages')
+  end
+
+  test "withdrawn case study" do
+    setup_and_visit_content_item('archived')
+
+    assert_has_component_title(@content_item["title"])
+    assert page.has_text?(@content_item["description"])
+
+    within ".withdrawal-notice" do
+      assert page.has_text?('This case study was withdrawn'), "is withdrawn"
+      within shared_component_selector("govspeak") do
+        assert_equal @content_item["details"]["withdrawn_notice"]["explanation"], JSON.parse(page.text).fetch("content")
+      end
+    end
+  end
+end
