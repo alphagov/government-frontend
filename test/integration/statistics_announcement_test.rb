272d5237ed20cd3f8f95c86fa1cb5108c6f374d6
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-01-28 12:27:07 +0000

    Improve Statistics Announcement intergration coveage
    Expand the 3 example types to cover the features of each type,
    comparing against the expected values in the schema examples.
    
    - Test description is shown. This feels a little repetitive,
      maybe there could be a per-format test that iterates the
      expectations common across all examples, eg title, description.
      These can be easily automated from the stubbed item, and would
      reduce the noise in per-example tests
    - High level tests that the right values are passed to the metadata
      component for both the dates/state, and the right labels are shown.
    - Check cancellation reason is shown, and scoped to the cancellation
      notice block (though it's unlikely the cancellation reason, or
      heading would appear elsewhere, being specific feels better).

--- test/integration/statistics_announcement_test.rb
+++ test/integration/statistics_announcement_test.rb
@@ -5,20 +5,42 @@ class StatisticsAnnouncementTest < ActionDispatch::IntegrationTest
     setup_and_visit_content_item('official_statistics')
 
     assert_has_component_title(@content_item["title"])
+    assert page.has_text?(@content_item["description"])
+    within shared_component_selector("metadata") do
+      assert page.has_text?('20 January 2016 9:30am (confirmed)'), "displays a confirmed release date"
+      assert page.has_text?('Release date'), "is laballed with a release date"
+    end
   end
 
   test "national statistics" do
     setup_and_visit_content_item('national_statistics')
 
     assert_has_component_title(@content_item["title"])
+    assert page.has_text?(@content_item["description"])
     assert page.has_css?('.national-statistics-logo img')
+
+    within shared_component_selector("metadata") do
+      assert page.has_text?('January 2016 (provisional)'), "displays a provisional release date"
+      assert page.has_text?('Release date'), "is laballed with a release date"
+    end
   end
 
   test "cancelled statistics" do
     setup_and_visit_content_item('cancelled_official_statistics')
 
     assert_has_component_title(@content_item["title"])
-    assert page.has_text?('Statistics release cancelled'), "is cancelled"
+    assert page.has_text?(@content_item["description"])
+    within '.cancellation-notice' do
+      assert page.has_text?('Statistics release cancelled'), "is cancelled"
+      assert page.has_text?(@content_item["details"]["cancellation_reason"]), "displays cancelleation reason"
+    end
+
+    within shared_component_selector("metadata") do
+      assert page.has_text?('20 January 2016 9:30am'), "displays the proposed release date"
+      assert page.has_text?('Proposed release'), "is laballed 'proposed release'"
+      assert page.has_text?('17 January 2016 2:19pm'), "displays the cancellation date"
+      assert page.has_text?('Cancellation date'), "is laballed as cancelled'"
+    end
   end
 
   def assert_has_component_title(title)
