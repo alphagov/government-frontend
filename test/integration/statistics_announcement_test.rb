0c65d99c66e1eb8edfe55422a2cd050cd97ea420
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-01-28 16:11:17 +0000

    Add integration test for Take Part
    Move the common setup and helper code out of Statistics Announcement
    test, so it can be used by Take Part, and future format tests.
    
    Infer the format from the test class name, so it doesn't need to be
    manually set in each test, but allow it to be overriden as a method
    if a test file's name doesn't match the convention.
    
    The tests added for take part aren't amazing, but they ensure that
    the integration helpers aren't linked to a single format, and
    give a basic example to add to later, if we need to.

--- test/integration/statistics_announcement_test.rb
+++ test/integration/statistics_announcement_test.rb
@@ -31,31 +31,4 @@ class StatisticsAnnouncementTest < ActionDispatch::IntegrationTest
     assert_has_component_metadata_pair("Proposed release", "20 January 2016 9:30am")
     assert_has_component_metadata_pair("Cancellation date", "17 January 2016 2:19pm")
   end
-
-  def assert_has_component_metadata_pair(label, value)
-    within shared_component_selector("metadata") do
-      # Flatten top level / "other" args, for consistent hash access
-      component_args = JSON.parse(page.text).tap do |args|
-        args.merge!(args.delete("other"))
-      end
-      assert_equal value, component_args.fetch(label)
-    end
-  end
-
-  def assert_has_component_title(title)
-    within shared_component_selector("title") do
-      assert_equal title, JSON.parse(page.text).fetch("title")
-    end
-  end
-
-  def setup_and_visit_content_item(name)
-    @content_item = JSON.parse(get_content_example(name)).tap do |item|
-      content_store_has_item(item["base_path"], item.to_json)
-      visit item["base_path"]
-    end
-  end
-
-  def get_content_example(name)
-    GovukContentSchemaTestHelpers::Examples.new.get('statistics_announcement', name)
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,4 +22,36 @@ class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
   include Slimmer::TestHelpers::SharedTemplates
+
+  def assert_has_component_metadata_pair(label, value)
+    within shared_component_selector("metadata") do
+      # Flatten top level / "other" args, for consistent hash access
+      component_args = JSON.parse(page.text).tap do |args|
+        args.merge!(args.delete("other"))
+      end
+      assert_equal value, component_args.fetch(label)
+    end
+  end
+
+  def assert_has_component_title(title)
+    within shared_component_selector("title") do
+      assert_equal title, JSON.parse(page.text).fetch("title")
+    end
+  end
+
+  def setup_and_visit_content_item(name)
+    @content_item = JSON.parse(get_content_example(name)).tap do |item|
+      content_store_has_item(item["base_path"], item.to_json)
+      visit item["base_path"]
+    end
+  end
+
+  def get_content_example(name)
+    GovukContentSchemaTestHelpers::Examples.new.get(schema_format, name)
+  end
+
+  # Override this method if your test file doesn't match the convention
+  def schema_format
+    self.class.to_s.gsub('Test', '').underscore
+  end
 end
