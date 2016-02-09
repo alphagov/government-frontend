9cedb6bc4437cee736b20e512cbd82242ca3bec7
Gemma Leigh <hello@gemmaleigh.com>
2016-02-09 11:45:54 +0000

    Customise the phase banner message for service manual guides
    Update the phase banner message for guide pages.

--- app/helpers/phase_label_helper.rb
+++ app/helpers/phase_label_helper.rb
@@ -1,7 +1,9 @@
 module PhaseLabelHelper
-  def render_phase_label(presented_object)
+  def render_phase_label(presented_object, message)
     if presented_object.respond_to?(:phase) && %w(alpha beta).include?(presented_object.phase)
-      render partial: "govuk_component/#{presented_object.phase}_label"
+      render partial: "govuk_component/#{presented_object.phase}_label", locals: {
+        message: message
+      }
     end
   end
 end
