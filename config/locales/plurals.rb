571aa3984650f35104d2f7de1daf6ae75e9831ff
Graham Pengelly <graham@three-tier.com>
2016-03-09 16:49:08 +0000

    Add missing pluralization rules
    This fixes broken rake tasks relating to translations (e.g. rake translation:regenerate)
    and should ensure proper rendering of plurals for languages not included in rails-i18n.
    
    There is a non ISO 639-1 code 'dr' which has been used for the
    Dari (Afghan Persian) language. This matches the locale used in Whitehall but
    should probably be 'prs' as per the ISO 639-3 standard.
    
    All of the additional rules have been set to one/other to match the
    available translations in the respective locale files.

---
+++ config/locales/plurals.rb
@@ -0,0 +1,26 @@
+{
+  #Dari - this isn't an iso code. Probably should be 'prs' as per ISO 639-3.
+  :dr => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Armenian
+  :hy => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Pushto
+  :ps => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Portugese
+  :pt => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Sinhalese
+  :si => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Somali
+  :so => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Albanian
+  :sq => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Tamil
+  :ta => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Turkmen
+  :tk => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Uzbek
+  :uz => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Chinese Hong Kong
+  :"zh-hk" => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+  #Chinese Taiwan
+  :"zh-tw" => { :i18n => { :plural => { :keys => [:one, :other], :rule => lambda { |n| n == 1 ? :one : :other } } } },
+}
