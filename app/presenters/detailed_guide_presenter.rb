8d0b4d016b58f337c0193b799648d966c610c15e
Paul Hayes <fofr@users.noreply.github.com>
2016-04-05 17:48:27 +0100

    Add metadata, footer, title context & breadcrumbs
    Port methods for metadata and footer from case study to identify
    differences in implementations. We should expect the same data to be
    provided in the same way.
    
    This can be DRY’d up later if the two are the same.

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -11,4 +11,71 @@ class DetailedGuidePresenter < ContentItemPresenter
       link_to(heading[:text], "##{heading[:id]}")
     end
   end
+
+  def context
+    parent["title"]
+  end
+
+  def breadcrumbs
+    [
+      { title: "Home", url: "/" },
+      { title: context, url: parent["base_path"] }
+    ]
+  end
+
+  def from
+    links("lead_organisations") + links("supporting_organisations") + links("worldwide_organisations")
+  end
+
+  def part_of
+    links("document_collections") + links("related_policies") + links("worldwide_priorities") + links("world_locations")
+  end
+
+  def history
+    return [] unless any_updates?
+    content_item["details"]["change_history"].map do |item|
+      {
+        display_time: display_time(item["public_timestamp"]),
+        note: item["note"],
+        timestamp: item["public_timestamp"]
+      }
+    end
+  end
+
+  def published
+    display_time(content_item["details"]["first_public_at"])
+  end
+
+  def updated
+    display_time(content_item["public_updated_at"]) if any_updates?
+  end
+
+  def short_history
+    if any_updates?
+      "#{I18n.t('content_item.metadata.updated')} #{updated}"
+    else
+      "#{I18n.t('content_item.metadata.published')} #{published}"
+    end
+  end
+
+private
+
+  def parent
+    content_item["links"]["parent"][0]
+  end
+
+  def any_updates?
+    if (first_public_at = content_item["details"]["first_public_at"]).present?
+      DateTime.parse(content_item["public_updated_at"]) != DateTime.parse(first_public_at)
+    else
+      false
+    end
+  end
+
+  def links(type)
+    return [] unless content_item["links"][type]
+    content_item["links"][type].map do |link|
+      link_to(link["title"], link["base_path"])
+    end
+  end
 end
