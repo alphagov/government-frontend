53e5b02e93e179366966dea01b6f743c88320f77
Matteo Grassotti <matteo.grassotti@digital.cabinet-office.gov.uk>
2016-04-14 14:01:46 +0200

    Complete metadata and footer sections
    - add the 'see all updates' link
    - remove the 'part of' section from the footer
    - complete the test

--- test/integration/detailed_guide_test.rb
+++ test/integration/detailed_guide_test.rb
@@ -8,7 +8,9 @@ class DetailedGuideTest < ActionDispatch::IntegrationTest
     assert page.has_text?(@content_item["description"])
 
     assert_has_component_metadata_pair("First published", "12 June 2014")
-    assert_has_component_metadata_pair("Last updated", "18 February 2016") #TODO: add "see all updates"
-    assert_has_component_metadata_pair("Part of", "PAYE") #TODO: add "and Business Tax"
+    assert_has_component_metadata_pair("Last updated", "18 February 2016, <a href=\"#history\">see all updates</a>")
+    link1 = "<a href=\"/topic/business-tax/paye\">PAYE</a>"
+    link2 = "<a href=\"/topic/business-tax\">Business tax</a>"
+    assert_has_component_metadata_pair("Part of", [link1, link2])
   end
 end
