bd2ed5a04a757826f2982984b58cbae84b8e225f
Tijmen Brommet <tijmen@gmail.com>
2016-05-17 14:23:42 +0100

    Fix reporting back to jenkins
    Because `INITIATING_REPO_NAME ` is not set in jenkins.sh, jenkins sends
    a message to GitHub about the wrong repository. This causes the
    gouk-content-schema branch tests to look like they’re not finished.

--- jenkins-schema.sh
+++ jenkins-schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPO_NAME="alphagov/govuk-content-schemas"
+export INITIATING_REPO_NAME="alphagov/govuk-content-schemas"
 export CONTEXT_MESSAGE="Verify government-frontend against content schemas"
 
 exec ./jenkins.sh
