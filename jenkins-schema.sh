1a26d139e20a273847bb79da9b33e1cbb098b9d3
Tijmen Brommet <tijmen@gmail.com>
2016-05-17 15:11:59 +0100

    Change to a known-working version of jenkins.sh
    So, argh. In previous commits I’ve changed the jenkins.sh to a more
    modern version, taken from the govuk_rails_template. This actually
    broke schema tests (because it always tested against
    govuk-content-schemas/master, not the thing being tested).
    
    This version is copied from collections-publisher, which currently
    works. I really hope that it works here too.

--- jenkins-schema.sh
+++ jenkins-schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export INITIATING_REPO_NAME="alphagov/govuk-content-schemas"
+export REPO_NAME="alphagov/govuk-content-schemas"
 export CONTEXT_MESSAGE="Verify government-frontend against content schemas"
 
 exec ./jenkins.sh
