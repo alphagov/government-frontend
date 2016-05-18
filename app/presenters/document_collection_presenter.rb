a9ac77c640f3d4d4944e50370119ee72808f8dc0
Paul Hayes <fofr@users.noreply.github.com>
2016-05-18 16:29:25 +0100

    Add content to document collections format
    * List each collection group with its ordered document list
    * List the contents of the collection based on groups
    * Generate an ID for each group heading, using hyphens to match the
    pattern in Whitehall and to keep existing fragment links working
    * List of each groups documents is simpler than on Whitehall because
    document type and date are not easily available yet

--- app/presenters/document_collection_presenter.rb
+++ app/presenters/document_collection_presenter.rb
@@ -1,2 +1,43 @@
 class DocumentCollectionPresenter < ContentItemPresenter
+  include Political
+  include Linkable
+  include Updatable
+  include Withdrawable
+  include ActionView::Helpers::UrlHelper
+
+  def contents
+    groups.map do |group|
+      title = group["title"]
+      link_to(title, "##{group_title_id(title)}")
+    end
+  end
+
+  def groups
+    content_item["details"]["collection_groups"]
+  end
+
+  def group_document_links(group)
+    group_documents(group).map do |link|
+      link_to(link["title"], link["base_path"])
+    end
+  end
+
+  def group_heading(group)
+    title = group["title"]
+    content_tag :h3, title, class: "group-title", id: group_title_id(title)
+  end
+
+private
+
+  def group_documents(group)
+    group["documents"].map { |id| documents_hash[id] }
+  end
+
+  def group_title_id(title)
+    title.tr(' ', '-').downcase
+  end
+
+  def documents_hash
+    @documents_hash ||= content_item["links"]["documents"].map { |d| [d["content_id"], d] }.to_h
+  end
 end
--- test/presenters/document_collection_presenter_test.rb
+++ test/presenters/document_collection_presenter_test.rb
@@ -4,4 +4,38 @@ class DocumentCollectionPresenterTest < PresenterTest
   def format_name
     "document_collection"
   end
+
+  test 'presents the basic details of a content item' do
+    assert_equal schema_item['description'], presented_item.description
+    assert_equal schema_item['format'], presented_item.format
+    assert_equal schema_item['title'], presented_item.title
+  end
+
+  test 'presents a contents list based on collection groups' do
+    contents = [
+      "<a href=\"#car-and-light-van\">Car and light van</a>",
+      "<a href=\"#moped-and-motorcycle\">Moped and motorcycle</a>",
+      "<a href=\"#lorry\">Lorry</a>",
+      "<a href=\"#bus-and-coach\">Bus and coach</a>",
+      "<a href=\"#driver-and-rider-trainer\">Driver and rider trainer</a>",
+      "<a href=\"#developed-driving-competence\">Developed driving competence</a>"
+    ]
+
+    assert_equal contents, presented_item.contents
+  end
+
+  test 'presents a group heading with generated ID' do
+    heading = '<h3 class="group-title" id="heading-with-spaces">Heading with Spaces</h3>'
+    assert_equal heading, presented_item.group_heading("title" => "Heading with Spaces")
+  end
+
+  test 'presents an ordered list of group documents' do
+    documents = [
+      "<a href=\"/government/publications/national-standard-for-driving-cars-and-light-vans\">National standard for driving cars and light vans</a>",
+      "<a href=\"/government/publications/car-and-small-van-driving-syllabus\">Car and light van driving syllabus</a>",
+      "<a href=\"/government/publications/car-and-light-van-driver-competence-framework\">Car and light van driver competence framework</a>",
+    ]
+    document_ids = schema_item["details"]["collection_groups"].first["documents"]
+    assert_equal documents, presented_item.group_document_links("documents" => document_ids)
+  end
 end
