f8026a56b3f3a01a5e18ee1bfb8535f2dd9ed3f7
Jack Scotti <jacopojackscotti@gmail.com>
2016-07-22 09:17:48 +0000

    Add breadcrumbs to items that can have parents

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -1,30 +1,14 @@
 class DetailedGuidePresenter < ContentItemPresenter
-  include Political
+  include Breadcrumbs
   include ExtractsHeadings
   include Linkable
-  include Updatable
   include NationalApplicability
+  include Political
+  include Updatable
   include ActionView::Helpers::UrlHelper
 
   def body
     content_item["details"]["body"]
-  end
-
-  def breadcrumbs
-    return [] unless parent
-
-    direct_parent = parent
-    ordered_parents = []
-
-    while direct_parent
-      ordered_parents.unshift(
-        title: direct_parent["title"],
-        url: direct_parent["base_path"],
-      )
-      direct_parent = direct_parent["parent"] && direct_parent["parent"].first
-    end
-
-    ordered_parents.unshift(title: "Home", url: "/")
   end
 
   def contents
