0571f839c4978ca26affa7ee6d5e15adb122b393
Paul Bowsher <paul.bowsher@gmail.com>
2016-08-05 12:41:18 +0100

    Add --live option to startup
    Easily run government-frontend against live content store and static

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 bundle install
-bundle exec rails s -p 3090
+
+if [[ $1 == "--live" ]] ; then
+  GOVUK_APP_DOMAIN=www.gov.uk \
+  GOVUK_WEBSITE_ROOT=https://www.gov.uk \
+  PLEK_SERVICE_CONTENT_STORE_URI=https://www.gov.uk/api \
+  PLEK_SERVICE_STATIC_URI=assets.publishing.service.gov.uk \
+  bundle exec rails s -p 3090
+else
+  bundle exec rails s -p 3090
+fi
