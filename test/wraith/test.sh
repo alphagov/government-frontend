b78cf69010a1d033480ccdea98250c42c756b790
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2016-09-01 11:21:19 +0000

    Serve gallery with python SimpleHTTPServer

--- test/wraith/test.sh
+++ test/wraith/test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if ! type phantomjs > /dev/null; then
-  echo 'PhantomJS was not found - installing...'
+  echo "PhantomJS was not found - installing..."
   sudo npm install --global phantomjs-prebuilt@2.1.7
 fi
 
 if ! type wraith > /dev/null; then
-  echo 'Wraith was not found installing...'
+  echo "Wraith was not found installing..."
   sudo gem install wraith -v 3.2.1
 fi
 
 current_checked_out_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-if [ $current_checked_out_branch == 'master' ]; then
-  echo 'You are on master, check out your feature branch and try again.'
+if [ $current_checked_out_branch == "master" ]; then
+  echo "You are on master, check out your feature branch and try again."
 else
   if [[ ! -z $(git status -s) ]]; then
     echo 'You have dirty changes, commit them then try again.'
@@ -26,6 +26,10 @@ else
     git checkout -
 
     wraith latest config.yaml
-    cd ../../
+
+    cd shots
+    current_machine_ip=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    echo "Serving gallery at http://$current_machine_ip:1234/gallery.html"
+    python -m SimpleHTTPServer 1234
   fi
 fi
