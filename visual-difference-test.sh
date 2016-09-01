6f4583dfa8cce029ecd8f5188a3383cd82ff157c
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2016-09-01 10:38:31 +0000

    Updates visual script to check branches and dirty changes

--- visual-difference-test.sh
+++ visual-difference-test.sh
@@ -5,17 +5,27 @@ if ! type phantomjs > /dev/null; then
   sudo npm install --global phantomjs-prebuilt@2.1.7
 fi
 
-if ! type wraith > /dev/null || ! wraith; then
+if ! type wraith > /dev/null; then
   echo 'Wraith was not found installing...'
   sudo gem install wraith -v 3.2.1
 fi
 
-git checkout master
+current_checked_out_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-cd test/wraith
-wraith history configs/history.yaml
+if [ $current_checked_out_branch == 'master' ]; then
+  echo 'You are on master, check out your feature branch and try again.'
+else
+  if [[ ! -z $(git status -s) ]]; then
+    echo 'You have dirty changes, commit them then try again.'
+  else
+    git checkout master
 
-git checkout -
+    cd test/wraith
+    wraith history configs/history.yaml
 
-wraith latest configs/history.yaml
-cd ../../
+    git checkout -
+
+    wraith latest configs/history.yaml
+    cd ../../
+  fi
+fi
