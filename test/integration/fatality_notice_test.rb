788458a91cfc89d60f21fba8db3eff8e1ac43d71
Graham Pengelly <graham@three-tier.com>
2016-09-15 11:31:03 +0100

    Add initial test for Fatality Notice

--- test/integration/fatality_notice_test.rb
+++ test/integration/fatality_notice_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
 class FatalityNoticeTest < ActionDispatch::IntegrationTest
+  test "typical fatality notice" do
+    setup_and_visit_content_item('fatality_notice')
+
+    assert_has_component_title("Sir George Pomeroy Colley killed in Boer War")
+
+    within(".description") do
+      assert_text <<-DESCRIPTION
+       It is with great sadness that the Ministry of Defense
+       must confirm that Sir George Pomeroy Colley, died in battle
+       in Zululand on 27 February 1881.
+       DESCRIPTION
+    end
+
+    assert_text("Operations in Zululand")
+  end
 end
