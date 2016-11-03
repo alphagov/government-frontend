f65639d09cef0b5fe1bae158eadfbe1938f81b5d
Andrew Garner <andrew@andrewgarner.com>
2016-11-03 08:42:29 +0000

    One assertion per DocumentCollectionPresenter test

--- test/presenters/document_collection_presenter_test.rb
+++ test/presenters/document_collection_presenter_test.rb
@@ -8,11 +8,24 @@ class DocumentCollectionPresenterTest
   end
 
   class PresentedDocumentCollection < TestCase
-    test 'presents the basic details of a content item' do
-      assert_equal schema_item['description'], presented_item.description
-      assert_equal schema_item['format'], presented_item.format
+    test 'presents the title' do
       assert_equal schema_item['title'], presented_item.title
-      assert_equal schema_item('document_collection_with_body')['details']['body'], presented_item('document_collection_with_body').body
+    end
+
+    test 'presents the description' do
+      assert_equal schema_item['description'], presented_item.description
+    end
+
+    test 'presents the format' do
+      assert_equal schema_item['format'], presented_item.format
+    end
+
+    test 'presents the body' do
+      example = 'document_collection_with_body'
+      expected_body = schema_item(example)['details']['body']
+      presented_body = presented_item(example).body
+
+      assert_equal expected_body, presented_body
     end
 
     test 'presents a contents list based on collection groups' do
@@ -30,6 +43,7 @@ class DocumentCollectionPresenterTest
 
     test 'presents a group heading with generated ID' do
       heading = '<h3 class="group-title" id="heading-with-spaces">Heading with Spaces</h3>'
+
       assert_equal heading, presented_item.group_heading("title" => "Heading with Spaces")
     end
 
@@ -43,6 +57,7 @@ class DocumentCollectionPresenterTest
         }
       ]
       document_ids = schema_item["details"]["collection_groups"].first["documents"]
+
       assert_equal documents, presented_item.group_document_links("documents" => [document_ids.first])
     end
   end
