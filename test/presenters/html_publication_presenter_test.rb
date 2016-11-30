6ce5b21e8bf6fedc51fd3d8b3ff4441a1e693887
Tijmen Brommet <tijmen@gmail.com>
2016-11-30 09:35:54 +0000

    Fix tests for presenter#format
    The content item attribute `format` is deprecated.
    
    https://github.com/alphagov/govuk-content-schemas/pull/444 attempts to
    remove `format` from the frontend, but the tests for this application
    fail without `format` in the examples. This is because the tests are
    testing kind of the wrong thing.
    
    For example:
    
    ```
    assert_equal schema_item['format'], presented_item.format
    ```
    
    Checks the string in `presented_item.format` which actually points to
    `schema_item['schema_name']`. Because they're `format` and
    `schema_name` are identical in the examples this worked. Now that we've
    removed `format` it doesn't anymore.

--- test/presenters/html_publication_presenter_test.rb
+++ test/presenters/html_publication_presenter_test.rb
@@ -6,7 +6,7 @@ class HtmlPublicationPresenterTest < PresenterTestCase
   end
 
   test 'presents the basic details of a content item' do
-    assert_equal schema_item("published")['format'], presented_item("published").format
+    assert_equal schema_item("published")['schema_name'], presented_item("published").format
     assert_equal schema_item("published")['links']['parent'][0]['document_type'], presented_item("published").format_sub_type
     assert_equal schema_item("published")['title'], presented_item("published").title
     assert_equal schema_item("published")['details']['body'], presented_item("published").body
