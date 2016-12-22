cb40126206c43e9e0a0895794d6b7cd93ddcb947
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2016-12-22 16:00:46 +0000

    Add World Location News Article presenter and view

---
+++ app/presenters/world_location_news_article_presenter.rb
@@ -0,0 +1,21 @@
+class WorldLocationNewsArticlePresenter < ContentItemPresenter
+  include ExtractsHeadings
+  include Political
+  include Withdrawable
+  include Linkable
+  include Updatable
+
+  def body
+    content_item["details"]["body"]
+  end
+
+  def contents
+    extract_headings_with_ids(body).map do |heading|
+      link_to(heading[:text], "##{heading[:id]}")
+    end
+  end
+
+  def image
+    content_item["details"]["image"]
+  end
+end
