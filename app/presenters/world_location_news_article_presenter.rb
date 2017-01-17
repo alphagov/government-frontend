9a0bcf4230c14d38fe6e71e8a4ee4a0a189e6911
Paul Hayes <fofr@users.noreply.github.com>
2017-01-17 14:48:21 +0000

    Remove unused parts of world news presenter
    * Contents aren’t used
    * Withdrawable is provided in the ContentItemPresenter
    * Use valid CSS selector in test

--- app/presenters/world_location_news_article_presenter.rb
+++ app/presenters/world_location_news_article_presenter.rb
@@ -1,7 +1,5 @@
 class WorldLocationNewsArticlePresenter < ContentItemPresenter
-  include ExtractsHeadings
   include Political
-  include Withdrawable
   include Linkable
   include Updatable
   include Shareable
@@ -10,12 +8,6 @@ class WorldLocationNewsArticlePresenter < ContentItemPresenter
 
   def body
     content_item["details"]["body"]
-  end
-
-  def contents
-    extract_headings_with_ids(body).map do |heading|
-      link_to(heading[:text], "##{heading[:id]}")
-    end
   end
 
   def image
