f0a2fb289b7df1a15812f2b641bb4f676e3c14a1
Paul Hayes <fofr@users.noreply.github.com>
2017-01-17 14:48:52 +0000

    Render news article format

--- app/presenters/news_article_presenter.rb
+++ app/presenters/news_article_presenter.rb
@@ -1,2 +1,16 @@
 class NewsArticlePresenter < ContentItemPresenter
+  include Political
+  include Linkable
+  include Updatable
+  include Shareable
+  include TitleAndContext
+  include Metadata
+
+  def body
+    content_item["details"]["body"]
+  end
+
+  def image
+    content_item["details"]["image"]
+  end
 end
--- test/presenters/news_article_presenter_test.rb
+++ test/presenters/news_article_presenter_test.rb
@@ -1,13 +1,69 @@
 require 'presenter_test_helper'
 
 class NewsArticlePresenterTest
-  class PresentedNewsArticle < PresenterTestCase
+  class NewsArticlePresenterTestCase < PresenterTestCase
+    attr_accessor :example_schema_name
+
     def format_name
       "news_article"
     end
+  end
 
-    test 'presents the format' do
-      assert_equal schema_item['format'], presented_item.format
+  class PresentedNewsArticleTest < NewsArticlePresenterTestCase
+    test 'is linkable' do
+      assert presented_item.is_a?(Linkable)
+    end
+
+    test 'is updatable' do
+      assert presented_item.is_a?(Updatable)
+    end
+
+    test 'is withdrawable' do
+      assert presented_item.is_a?(Withdrawable)
+    end
+
+    test 'is shareable' do
+      assert presented_item.is_a?(Shareable)
+    end
+
+    test 'includes political' do
+      assert presented_item.is_a?(Political)
+    end
+
+    test 'presents a description' do
+      assert_equal schema_item['description'], presented_item.description
+    end
+
+    test 'presents a body' do
+      assert_equal schema_item['details']['body'], presented_item.body
+    end
+
+    test 'presents a readable first published date' do
+      assert_equal '25 December 2016', presented_item.published
+    end
+
+    test 'presents the locale' do
+      assert_equal schema_item['locale'], presented_item.locale
+    end
+  end
+
+  class HistoryModePresentedNewsArticle < NewsArticlePresenterTestCase
+    def example_schema_name
+      "news_article_history_mode"
+    end
+
+    test 'presents historically political' do
+      assert presented_item(example_schema_name).historically_political?
+    end
+  end
+
+  class TranslatedPresentedNewsArticle < NewsArticlePresenterTestCase
+    def example_schema_name
+      "news_article_news_story_translated_arabic"
+    end
+
+    test 'presents the locale as the translated item locale' do
+      assert_equal 'ur', presented_item(example_schema_name).locale
     end
   end
 end
