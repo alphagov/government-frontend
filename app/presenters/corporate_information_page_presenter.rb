c71aabbc3da7099b95e9fbae40139b2081ad5075
Paul Hayes <fofr@users.noreply.github.com>
2017-01-26 16:25:02 +0000

    Use organisation in details as default
    Rather than picking the first organisation in an unordered list of
    organisations, use the guid defined for the purpose that’s in the
    details.
    
    * Raise an error if the referenced organisation isn’t in the links hash
    
    Without an organisation the page can’t render the logo or branding. It
    would not be clear which organisation is being referred to.

--- app/presenters/corporate_information_page_presenter.rb
+++ app/presenters/corporate_information_page_presenter.rb
@@ -5,4 +5,15 @@ class CorporateInformationPagePresenter < ContentItemPresenter
   def body
     content_item["details"]["body"]
   end
+
+  def default_organisation
+    organisation_content_id = content_item["details"]["organisation"]
+    organisation = content_item["links"]["organisations"].detect do |org|
+      org["content_id"] == organisation_content_id
+    end
+
+    raise "No organisation in links that matches the one specified in details: #{organisation_content_id}" unless organisation
+
+    organisation
+  end
 end
