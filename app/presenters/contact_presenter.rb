7aeb4c1652eda99fca0ac1743974f21c51922d26
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-02-15 14:50:43 +0000

    Add related items to contact format
    - Added CSS to balance the related links component correctly, potentially we could avoid this by spiking unidrectional vertical rhythm.

--- app/presenters/contact_presenter.rb
+++ app/presenters/contact_presenter.rb
@@ -1,2 +1,34 @@
 class ContactPresenter < ContentItemPresenter
+  def related_items
+    [
+      {
+        title: "Elsewhere on GOV.UK",
+        items: quick_links
+      },
+      {
+        title: "Other contacts",
+        items: related_contacts_links
+      }
+    ]
+  end
+
+private
+
+  def related_contacts_links
+    content_item["links"]["related"].map do |link|
+      {
+        title: link["title"],
+        url:  link["base_path"]
+      }
+    end
+  end
+
+  def quick_links
+    content_item["details"]["quick_links"].map do |link|
+      {
+        title: link["title"],
+        url:  link["url"]
+      }
+    end
+  end
 end
--- test/presenters/contact_presenter_test.rb
+++ test/presenters/contact_presenter_test.rb
@@ -9,5 +9,21 @@ class ContactPresenterTest
     test 'presents the format' do
       assert_equal schema_item['format'], presented_item.format
     end
+
+    test 'presents the quick links in related items' do
+      first_quick_link = schema_item['details']['quick_links'].first
+      first_presented_quick_link = presented_item.related_items.first[:items].first
+
+      assert_equal first_quick_link['title'], first_presented_quick_link[:title]
+      assert_equal 'Elsewhere on GOV.UK', presented_item.related_items.first[:title]
+    end
+
+    test 'presents the related contacts links in related items' do
+      first_related_contact_link = schema_item['links']['related'].first
+      first_presented_contact_link = presented_item.related_items.last[:items].first
+
+      assert_equal first_related_contact_link['title'], first_presented_contact_link[:title]
+      assert_equal 'Other contacts', presented_item.related_items.last[:title]
+    end
   end
 end
