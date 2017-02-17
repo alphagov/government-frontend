978eb38d9cc334a35199194fc807bce132fba6d3
Paul Hayes <fofr@users.noreply.github.com>
2017-02-17 15:39:04 +0000

    Render travel advice related items and title
    * Use component
    * Keep logic for component parameters in presenter
    * Use country name as page title

--- app/presenters/travel_advice_presenter.rb
+++ app/presenters/travel_advice_presenter.rb
@@ -1,2 +1,35 @@
 class TravelAdvicePresenter < ContentItemPresenter
+  def title_and_context
+    {
+      context: 'Foreign travel advice',
+      title: country_name
+    }
+  end
+
+  def related_items
+    items = ordered_related_items.map do |link|
+      {
+        title: link["title"],
+        url:  link["base_path"]
+      }
+    end
+
+    {
+      sections: [
+        {
+          title: 'Elsewhere on GOV.UK',
+          items: items
+        }
+      ]
+    }
+  end
+
+private
+
+  def country_name
+    content_item["details"]["country"]["name"]
+  end
+  def ordered_related_items
+    content_item["links"]["ordered_related_items"] || []
+  end
 end
