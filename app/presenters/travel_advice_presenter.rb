d913ffbad2db199c570b40347f5087aa6df6c756
Paul Hayes <fofr@users.noreply.github.com>
2017-02-17 15:45:31 +0000

    Add travel advice part navigation
    * Ported from multipage frontend
    * Doesn’t include highlighting of current part
    * Simplify view by keeping logic in presenter
    * Use grid classes for two column layout

--- app/presenters/travel_advice_presenter.rb
+++ app/presenters/travel_advice_presenter.rb
@@ -24,11 +24,49 @@ class TravelAdvicePresenter < ContentItemPresenter
     }
   end
 
+  def parts_navigation
+    part_links.each_slice(part_navigation_group_size).to_a
+  end
+
+  def parts_navigation_second_list_start
+    part_navigation_group_size + 1
+  end
+
 private
 
   def country_name
     content_item["details"]["country"]["name"]
   end
+
+  def parts
+    content_item["details"]["parts"] || []
+  end
+
+  def part_links
+    links = [
+      {
+        title: 'Current travel advice',
+        path: content_item["base_path"]
+      }
+    ]
+
+    links + parts.map do |part|
+      {
+        title: part['title'],
+        path: "#{content_item["base_path"]}/#{part['slug']}"
+      }
+    end
+  end
+
+  def part_navigation_group_size
+    size = part_links.size.to_f / 2
+    if size < 2
+      3
+    else
+      size.ceil
+    end
+  end
+
   def ordered_related_items
     content_item["links"]["ordered_related_items"] || []
   end
--- test/presenters/travel_advice_presenter_test.rb
+++ test/presenters/travel_advice_presenter_test.rb
@@ -6,8 +6,42 @@ class TravelAdvicePresenterTest
       "travel_advice"
     end
 
-    test 'presents the format' do
-      assert_equal schema_item['format'], presented_item.format
+    test "the summary is included as the first navigation item" do
+      base_path = schema_item("full-country")["base_path"]
+      first_nav_item = presented_item("full-country").parts_navigation.first.first
+
+      assert_equal base_path, first_nav_item[:path]
+      assert_equal 'Current travel advice', first_nav_item[:title]
     end
-  end
+
+    test "navigation items link to all parts" do
+      parts = schema_item("full-country")["details"]["parts"]
+      part_links = presented_item("full-country").parts_navigation.flatten
+
+      assert_equal parts.size + 1, part_links.size
+      assert_equal parts[0]['title'], part_links[1][:title]
+      assert part_links[1][:path].include?(parts[0]['slug'])
+    end
+
+    test "part navigation is in one group when 3 or fewer navigation items (2 parts + summary)" do
+      example = schema_item("full-country")
+
+      example["details"]["parts"] = example["details"]["parts"].first(3)
+      assert_equal 2, presented_item("full-country", example).parts_navigation.size
+
+      example["details"]["parts"] = example["details"]["parts"].first(2)
+      assert_equal 1, presented_item("full-country", example).parts_navigation.size
+
+      example["details"]["parts"] = []
+      assert_equal 1, presented_item("full-country", example).parts_navigation.size
+    end
+
+    test "part navigation is split into two groups" do
+      assert_equal 2, presented_item("full-country").parts_navigation.size
+    end
+
+    test "the second list of parts starting index continues on from the first list" do
+      presented = presented_item("full-country")
+      assert_equal presented.parts_navigation.first.size + 1, presented.parts_navigation_second_list_start
+    end
 end
