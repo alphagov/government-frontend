5ade0b06e09076b093d65842f21c0cf4ae6bb2ac
Carlos Vilhena <carlosvilhena@gmail.com>
2017-03-03 17:05:49 +0000

    Remove the New Navigation feature flag
    This means we can toggle the A/B variant in production via the chrome
    extension.  When we rollout the new navigation with a Fastly
    configuration change, the % of users we select will start seeing these
    new pages.

--- app/models/education_navigation_ab_test_request.rb
+++ app/models/education_navigation_ab_test_request.rb
@@ -12,7 +12,7 @@ class EducationNavigationAbTestRequest
   end
 
   def ab_test_applies?
-    new_navigation_enabled? && content_is_tagged_to_a_taxon? && content_schema_has_new_navigation?
+    content_is_tagged_to_a_taxon? && content_schema_has_new_navigation?
   end
 
   def should_present_new_navigation_view?
@@ -24,10 +24,6 @@ class EducationNavigationAbTestRequest
   end
 
 private
-
-  def new_navigation_enabled?
-    ENV['ENABLE_NEW_NAVIGATION'] == 'yes'
-  end
 
   def content_is_tagged_to_a_taxon?
     @content_item.dig("links", "taxons").present?
