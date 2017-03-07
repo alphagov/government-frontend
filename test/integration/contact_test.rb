7dd68398a794e38c012e632fb3e2bf78699cbe04
Paul Hayes <fofr@users.noreply.github.com>
2017-03-07 16:34:02 +0000

    Only render phone headings when multiple
    When there is only one number, then the content heading is sufficient
    to describe it.

--- test/integration/contact_test.rb
+++ test/integration/contact_test.rb
@@ -31,13 +31,24 @@ class ContactTest < ActionDispatch::IntegrationTest
   test "phones are rendered" do
     setup_and_visit_content_item('contact')
     within_component_govspeak do |component_args|
-      content = component_args.fetch("content")
-      first_phone = @content_item["details"]["phone_numbers"].first["number"]
+      first_phone = @content_item["details"]["phone_numbers"].first
+      html = Nokogiri::HTML.parse(component_args.fetch("content"))
 
-      html = Nokogiri::HTML.parse(content)
       assert_not_nil html.at_css("h2#phone-title")
-      assert_not_nil html.at("p:contains(\"#{first_phone}\")")
+      assert_not_nil html.at("h3:contains(\"#{first_phone['title']}\")")
+      assert_not_nil html.at("p:contains(\"#{first_phone['number']}\")")
       assert_not_nil html.at("p:contains(\"24 hours a day, 7 days a week\")")
+    end
+  end
+
+  test "phone number heading is not rendered when only one number" do
+    setup_and_visit_content_item('contact_with_welsh')
+    within_component_govspeak do |component_args|
+      first_phone = @content_item["details"]["phone_numbers"].first
+      html = Nokogiri::HTML.parse(component_args.fetch("content"))
+
+      assert_equal 1, @content_item["details"]["phone_numbers"].size
+      refute html.at("h3:contains(\"#{first_phone['title']}\")")
     end
   end
 
