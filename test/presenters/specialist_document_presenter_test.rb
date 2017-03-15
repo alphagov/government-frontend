f0c383a5cab4883247f134d96aefdbaa67675500
Paul Hayes <fofr@users.noreply.github.com>
2017-03-15 17:34:20 +0000

    Include facet values in metadata and footer
    * Merge facet values within details[‘metadata’] with facets provided by
    the single finder in the links hash
    * When text and no allowed values, display text value
    * When text with allowed values, convert to friendly version
    * When a text field with allowed values is filterable, link to the
    filtered results
    * When showing a date facet, render a friendly date
    
    Examples can’t be updated with facets because of:
    https://github.com/alphagov/govuk-content-schemas/issues/453
    
    * Use test fixtures instead of examples for now

--- test/presenters/specialist_document_presenter_test.rb
+++ test/presenters/specialist_document_presenter_test.rb
@@ -58,4 +58,108 @@ class SpecialistDocumentPresenterTest
       assert_equal title_component_params, presented_item('aaib-reports').title_and_context
     end
   end
+
+  class PresentedSpecialistDocumentWithFinderFacets < SpecialistDocumentTestCase
+    def example_with_finder_facets(facets = [], values = {})
+      example = schema_item('aaib-reports')
+      example_finder = {
+        "base_path" => "/finder-base-path",
+        "title" => "Finder title",
+        "details" => {
+          "document_noun" => "case",
+          "filter" => {
+            "document_type" => "cma_case"
+          },
+          "format_name" => "Competition and Markets Authority case",
+          "facets" => facets,
+        },
+      }
+
+      example['details']['metadata'] = values
+      example['links']['finder'] = [example_finder]
+      example
+    end
+
+    def example_facet(overrides = {})
+      {
+        "name" => "Facet name",
+        "key" => "facet-key",
+        "type" => "text",
+        "filterable" => false
+      }.merge(overrides)
+    end
+
+    test 'includes non-filterable facet as text in metadata' do
+      values = { "facet-key" => "document-value" }
+      example = example_with_finder_facets([example_facet], values)
+
+      presented_metadata = present_example(example).metadata[:other]
+      assert_equal "document-value", presented_metadata["Facet name"]
+    end
+
+    test 'includes friendly label for facet value in metadata' do
+      overrides = {
+        "allowed_values" => [
+          {
+            "label" => "Document value from label",
+            "value" => "document-value"
+          }
+        ]
+      }
+
+      values = { "facet-key" => "document-value" }
+      example = example_with_finder_facets([example_facet(overrides)], values)
+
+      presented_metadata = present_example(example).metadata[:other]
+      assert_equal "Document value from label", presented_metadata["Facet name"]
+    end
+
+    test 'handles multiple values for facets' do
+      overrides = {
+        "allowed_values" => [
+          {
+            "label" => "One",
+            "value" => "one"
+          },
+          {
+            "label" => "Two",
+            "value" => "two"
+          }
+        ]
+      }
+
+      values = { "facet-key" => %w{one two} }
+      example = example_with_finder_facets([example_facet(overrides)], values)
+
+      presented_metadata = present_example(example).metadata[:other]
+      assert_equal "One, Two", presented_metadata["Facet name"]
+    end
+
+    test 'creates links for filterable friendly values' do
+      overrides = {
+        "filterable" => true,
+        "allowed_values" => [
+          {
+            "label" => "Something",
+            "value" => "something"
+          }
+        ]
+      }
+
+      values = { "facet-key" => "something" }
+      example = example_with_finder_facets([example_facet(overrides)], values)
+
+      presented_metadata = present_example(example).metadata[:other]
+      assert_equal "<a href=\"/finder-base-path?facet-key%5B%5D=something\">Something</a>", presented_metadata["Facet name"]
+    end
+
+    test 'includes friendly dates for date facets in metadata' do
+      overrides = { "type" => "date" }
+      values = { "facet-key" => "2010-01-01" }
+      example = example_with_finder_facets([example_facet(overrides)], values)
+
+      presented_metadata = present_example(example).metadata[:other]
+      assert_equal "1 January 2010", presented_metadata["Facet name"]
+    end
+  end
 end
