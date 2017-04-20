10ac098eb69dfb138f4ea4c1718b38d74d09ab40
Paul Hayes <fofr@users.noreply.github.com>
2017-04-20 14:06:10 +0100

    Render guides
    * Add parts navigation
    * Include related items from govuk_navigation_helper
    * Include link to print guide

--- app/presenters/guide_presenter.rb
+++ app/presenters/guide_presenter.rb
@@ -1,2 +1,27 @@
 class GuidePresenter < ContentItemPresenter
+  def page_title
+    if @part_slug
+      "#{title}: #{current_part_title}"
+    else
+      title
+    end
+  end
+
+  def current_part_title_with_index
+    "#{current_part_index + 1}. #{current_part_title}"
+  end
+
+  def print_link
+    "#{base_path}/print"
+  end
+
+  def related_items
+    @nav_helper.related_items
+  end
+
+private
+
+  def current_part_index
+    parts.index(current_part)
+  end
 end
--- test/presenters/guide_presenter_test.rb
+++ test/presenters/guide_presenter_test.rb
@@ -6,8 +6,32 @@ class GuidePresenterTest
       "guide"
     end
 
-    test 'presents the format' do
-      assert_equal schema_item['format'], presented_item.format
+    test "presents unique page titles for each part" do
+      assert_equal presented_item.page_title, schema_item['title']
+      schema_item['details']['parts'].each do |part|
+        assert_equal presented_item('guide', part['slug']).page_title, "#{schema_item['title']}: #{part['title']}"
+      end
+    end
+
+    test "presents part titles with their index" do
+      first_part_title = schema_item['details']['parts'].first['title']
+      assert_equal presented_item.current_part_title_with_index, "1. #{first_part_title}"
+    end
+
+    test "presents a print link" do
+      assert_equal "#{schema_item['base_path']}/print", presented_item.print_link
+    end
+
+  private
+
+    def presented_item(type = format_name, part_slug = nil, overrides = {})
+      schema_example_content_item = schema_item(type)
+      part_slug = "/#{part_slug}" if part_slug
+
+      GuidePresenter.new(
+        schema_example_content_item.merge(overrides),
+        "#{schema_example_content_item['base_path']}#{part_slug}"
+      )
     end
   end
 end
