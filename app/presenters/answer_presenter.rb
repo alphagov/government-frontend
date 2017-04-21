7665eefc4652953c81c929b205a6c4edcf2cd06b
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2017-04-21 14:57:50 +0100

    Add updated at to the answers page

--- app/presenters/answer_presenter.rb
+++ app/presenters/answer_presenter.rb
@@ -4,4 +4,8 @@ class AnswerPresenter < ContentItemPresenter
   def body
     content_item["details"]["body"]
   end
+
+  def updated
+    display_date(content_item["public_updated_at"]) if content_item["public_updated_at"]
+  end
 end
