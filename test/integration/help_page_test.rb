6b3c28d06d8a3ac0777ab8379505ec7afc660b7e
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2017-04-27 11:58:18 +0100

    Temporarily remove the last updated at
    The public updated at is incorrect currently, has been decided to move for the moment

--- test/integration/help_page_test.rb
+++ test/integration/help_page_test.rb
@@ -5,7 +5,7 @@ class HelpPageTest < ActionDispatch::IntegrationTest
     setup_and_visit_content_item('help_page')
     assert page.has_text?(@content_item["title"])
 
-    assert page.has_text?("Last updated: 16 December 2014")
+    # assert page.has_text?("Last updated: 16 December 2014")
     assert_has_component_govspeak(@content_item["details"]["body"].squish)
   end
 
