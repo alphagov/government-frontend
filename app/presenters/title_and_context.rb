92d2994dab2d2819cfae02c3371f3166836d2e99
Paul Hayes <fofr@users.noreply.github.com>
2017-05-17 14:24:07 +0100

    Rename translations from format to schema_name

--- app/presenters/title_and_context.rb
+++ app/presenters/title_and_context.rb
@@ -2,7 +2,7 @@ module TitleAndContext
   def title_and_context
     {
       title: title,
-      context: I18n.t("content_item.format.#{document_type}", count: 1),
+      context: I18n.t("content_item.schema_name.#{document_type}", count: 1),
       average_title_length: "long"
     }
   end
--- test/controllers/content_items_controller_test.rb
+++ test/controllers/content_items_controller_test.rb
@@ -101,7 +101,7 @@ class ContentItemsControllerTest < ActionController::TestCase
   test "renders translated content items in their locale" do
     content_item = content_store_has_schema_example('case_study', 'translated')
     locale = content_item['locale']
-    translated_format_name = I18n.t("content_item.format.case_study", count: 1, locale: locale)
+    translated_schema_name = I18n.t("content_item.schema_name.case_study", count: 1, locale: locale)
 
     get :show, params: { path: path_for(content_item, locale), locale: locale }
 
