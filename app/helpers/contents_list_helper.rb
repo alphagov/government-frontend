2077f7bba5c688100d737d805c1857fd9ecd600c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-06-23 14:33:08 +0000

    Refactoring code

--- app/helpers/contents_list_helper.rb
+++ app/helpers/contents_list_helper.rb
@@ -1,14 +1,13 @@
 module ContentsListHelper
-  def insert_spans(text)
-    no_tags = strip_tags(text) #just the text of the link
-    number = /^[0-9]*[.]/.match(no_tags)
+  def wrap_numbers_with_spans(content_item_link)
+    content_item_text = strip_tags(content_item_link) #just the text of the link
+    number = /^[0-9]*[.]/.match(content_item_text)
 
     if number
-      link_text = no_tags.sub number.to_s, '' #remove the number from the text
-      text.sub! no_tags, '<span class="contents-number">' + number.to_s + '</span><span class="contents-text">' + link_text.strip + '</span>'
-      text = text.squish.html_safe
+      words = content_item_text.sub(number.to_s, '').strip #remove the number from the text
+      content_item_link.sub(content_item_text, "<span class=\"contents-number\">#{number}</span><span class=\"contents-text\">#{words}</span>").squish.html_safe
+    else
+      content_item_link
     end
-
-    text
   end
 end
