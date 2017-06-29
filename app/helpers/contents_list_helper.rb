d42a6fe35211c09461c6952203b53330442d030e
Paul Hayes <fofr@users.noreply.github.com>
2017-06-29 12:51:14 +0100

    Allow HTML pub contents to work with custom numbers
    Publishers can enter custom numbers. Add support for the forms:
    
    * Numbers without periods (1)
    * Numbers with decimals (1.2)
    * Allow long numbers to be spaced correctly and not wrap badly (100.)
    * Ignore numbers bigger than 999 (eg 2014)
    * Ignore contents items that are just numbers

--- app/helpers/contents_list_helper.rb
+++ app/helpers/contents_list_helper.rb
@@ -1,7 +1,13 @@
 module ContentsListHelper
   def wrap_numbers_with_spans(content_item_link)
     content_item_text = strip_tags(content_item_link) #just the text of the link
-    number = /^[0-9]*[.]/.match(content_item_text)
+
+    # Must start with a number
+    # Number must be between 1 and 999 (ie not 2014)
+    # Must be followed by a space
+    # May contain a period `1.`
+    # May be a decimal `1.2`
+    number = /^\d{1,3}(\.?|\.\d{1,2})(?=\s)/.match(content_item_text)
 
     if number
       words = content_item_text.sub(number.to_s, '').strip #remove the number from the text
