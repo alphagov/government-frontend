e6248093c1627e5b371734f6851250309fb9f9ae
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2017-07-21 14:28:55 +0000

    Create download link component
    Replaces the existing download map link in travel advice pages.
    Replaces download icon (background image) with inline svg to remove need for
    min-height and get more consistent focus style

---
+++ test/components/download_link_test.rb
@@ -0,0 +1,23 @@
+require 'component_test_helper'
+
+class DownloadLinkTest < ComponentTestCase
+  def component_name
+    "download-link"
+  end
+
+  test "fails to render a download link when no href is given" do
+    assert_raise do
+      render_component({})
+    end
+  end
+
+  test "renders a download link correctly" do
+    render_component(href: '/download-me')
+    assert_select ".app-c-download-link[href=\"/download-me\"]"
+  end
+
+  test "renders a download link with custom link text correctly" do
+    render_component(href: '/download-map', link_text: 'Download this file')
+    assert_select ".app-c-download-link[href=\"/download-map\"]", text: "Download this file"
+  end
+end
