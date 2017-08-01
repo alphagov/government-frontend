155e35cf38800d05ba05a8bc302b36c57edc502e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2017-08-01 09:04:48 +0000

    Add more tests based on fixtures
    Adding more realistic test cases based on translation component fixtures

--- test/components/translation_nav_test.rb
+++ test/components/translation_nav_test.rb
@@ -11,7 +11,7 @@ class TranslationNavTest < ComponentTestCase
     end
   end
 
-  test "renders an active translation nav item with only text description" do
+  test "renders an active translation nav item with a text description" do
     render_component(
       translations: [
         {
@@ -19,27 +19,77 @@ class TranslationNavTest < ComponentTestCase
           base_path: '/en',
           text: 'English',
           active: true
+        },
+        {
+          locale: 'hi',
+          base_path: '/hi',
+          text: 'हिंदी',
+        }
+      ]
+    )
+
+    assert_select ".app-c-translation-nav__list__language", text: "English"
+
+    assert_select ".app-c-translation-nav__list__language a[lang=\"hi\"]", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]"
+  end
+
+  test "does not render an active translation item with a link" do
+    render_component(
+      translations: [
+        {
+          locale: 'en',
+          base_path: '/en',
+          text: 'English',
+          active: true
+        },
+        {
+          locale: 'hi',
+          base_path: '/hi',
+          text: 'हिंदी',
         }
       ]
     )
 
     assert_select ".app-c-translation-nav__list__language", text: "English"
     assert_select ".app-c-translation-nav__list__language a[href=\"/en\"]",
-                    false, "An active item should not link to a page translation"
+      false, "An active item should not link to a page translation"
+
+
+    assert_select ".app-c-translation-nav__list__language a[lang=\"hi\"]", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]"
   end
 
-  test "renders an inactive translation nav item with href, base path and text" do
+  test "renders an inactive translation nav item with locale, base path and text" do
     render_component(
       translations: [
         {
           locale: 'fr',
           base_path: '/fr',
-          text: 'French'
+          text: 'French',
+        },
+        {
+          locale: 'hi',
+          base_path: '/hi',
+          text: 'हिंदी',
+          active: true
+        },
+        {
+          locale: 'zh',
+          base_path: '/zh',
+          text: '中文'
         }
       ]
     )
 
     assert_select ".app-c-translation-nav__list__language a[lang=\"fr\"]", text: "French"
     assert_select ".app-c-translation-nav__list__language a[href=\"/fr\"]"
+
+    assert_select ".app-c-translation-nav__list__language a[lang=\"zh\"]", text: "中文"
+    assert_select ".app-c-translation-nav__list__language a[href=\"/zh\"]"
+
+    assert_select ".app-c-translation-nav__list__language", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]",
+      false, "An active item should not link to a page translation"
   end
 end
