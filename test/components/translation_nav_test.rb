f0d7e3ad948506d1ec01bb14f5cad4f3b2f3bc96
Paul Hayes <fofr@users.noreply.github.com>
2017-08-17 16:37:37 +0100

    Remove double double underscores from selector
    See guidance on BEM:
    http://getbem.com/faq/#what-would-be-a-class-name-for-an-element-inside-
    another-element-block__el1__el2-
    
    > block structure should be flattened; you do not need to reflect
    nested DOM structure of the block

--- test/components/translation_nav_test.rb
+++ test/components/translation_nav_test.rb
@@ -5,6 +5,22 @@ class TranslationNavTest < ComponentTestCase
     "translation-nav"
   end
 
+  def multiple_translations
+    [
+      {
+        locale: 'en',
+        base_path: '/en',
+        text: 'English',
+        active: true
+      },
+      {
+        locale: 'hi',
+        base_path: '/hi',
+        text: 'हिंदी',
+      }
+    ]
+  end
+
   test "renders nothing when no translations are given" do
     assert_empty render_component({})
   end
@@ -23,52 +39,24 @@ class TranslationNavTest < ComponentTestCase
   end
 
   test "renders an active translation nav item with a text description" do
-    render_component(
-      translations: [
-        {
-          locale: 'en',
-          base_path: '/en',
-          text: 'English',
-          active: true
-        },
-        {
-          locale: 'hi',
-          base_path: '/hi',
-          text: 'हिंदी',
-        }
-      ]
-    )
+    render_component(translations: multiple_translations)
 
-    assert_select ".app-c-translation-nav__list__language", text: "English"
+    assert_select ".app-c-translation-nav__list-item", text: "English"
 
-    assert_select ".app-c-translation-nav__list__language a[lang=\"hi\"]", text: "हिंदी"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]"
+    assert_select ".app-c-translation-nav__list-item a[lang=\"hi\"]", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/hi\"]"
   end
 
   test "does not render an active translation item with a link" do
-    render_component(
-      translations: [
-        {
-          locale: 'en',
-          base_path: '/en',
-          text: 'English',
-          active: true
-        },
-        {
-          locale: 'hi',
-          base_path: '/hi',
-          text: 'हिंदी',
-        }
-      ]
-    )
+    render_component(translations: multiple_translations)
 
-    assert_select ".app-c-translation-nav__list__language", text: "English"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/en\"]",
+    assert_select ".app-c-translation-nav__list-item", text: "English"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/en\"]",
       false, "An active item should not link to a page translation"
 
 
-    assert_select ".app-c-translation-nav__list__language a[lang=\"hi\"]", text: "हिंदी"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]"
+    assert_select ".app-c-translation-nav__list-item a[lang=\"hi\"]", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/hi\"]"
   end
 
   test "renders an inactive translation nav item with locale, base path and text" do
@@ -93,14 +81,14 @@ class TranslationNavTest < ComponentTestCase
       ]
     )
 
-    assert_select ".app-c-translation-nav__list__language a[lang=\"fr\"]", text: "French"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/fr\"]"
+    assert_select ".app-c-translation-nav__list-item a[lang=\"fr\"]", text: "French"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/fr\"]"
 
-    assert_select ".app-c-translation-nav__list__language a[lang=\"zh\"]", text: "中文"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/zh\"]"
+    assert_select ".app-c-translation-nav__list-item a[lang=\"zh\"]", text: "中文"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/zh\"]"
 
-    assert_select ".app-c-translation-nav__list__language", text: "हिंदी"
-    assert_select ".app-c-translation-nav__list__language a[href=\"/hi\"]",
+    assert_select ".app-c-translation-nav__list-item", text: "हिंदी"
+    assert_select ".app-c-translation-nav__list-item a[href=\"/hi\"]",
       false, "An active item should not link to a page translation"
   end
 end
