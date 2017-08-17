ce0b41fef5acb19a554bc1d04d9f242c66d61bc9
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-08-17 15:02:41 +0100

    Add test to visit all components
    This will catch any JavaScript errors on the page, which will be
    useful when we throw accessibility related errors

---
+++ test/components/all_components.rb
@@ -0,0 +1,18 @@
+require 'component_test_helper'
+
+# TODO: Move this logic to the GOVUK Publishing Components gem
+# and have the tests visit each component.
+class AllComponentsTest < ActionDispatch::IntegrationTest
+  test "renders all component guide pages without erroring" do
+    visit '/component-guide'
+    components = all(:css, '.component-list a').map do |el|
+      {
+        text: el[:text],
+        href: el[:href]
+      }
+    end
+    components.each do |component|
+      visit component[:href] + '/preview'
+    end
+  end
+end
