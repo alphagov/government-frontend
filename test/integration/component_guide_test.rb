41f627acc3bf967c551b7a898e8197cd47eb744b
Paul Hayes <fofr@users.noreply.github.com>
2017-09-18 14:04:47 +0100

    Use integration tests from component gem
    * Bump gem to 1.8.0
    * Run component guide integration tests, including automated
    accessibility tests
    * See: https://github.com/alphagov/govuk_publishing_components/pull/68

---
+++ test/integration/component_guide_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+require 'govuk_publishing_components/minitest/component_guide_test'
+
+class ComponentGuideTest < ActionDispatch::IntegrationTest
+  include GovukPublishingComponents::Minitest::ComponentGuideTest
+end
