bcf8a485913b9d026d89162958e449da16500447
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-10-06 14:24:05 +0100

    Prevent widows in forthcoming stats notice title
    Ensure notices insert an `nbsp` between the last two words in the title
    using the typography helper.
    Update regex to handle times as the last word (e.g. 9:30)

--- app/helpers/typography_helper.rb
+++ app/helpers/typography_helper.rb
@@ -2,7 +2,7 @@ module TypographyHelper
   def nbsp_between_last_two_words(text)
     return text if text.nil?
     escaped_text = html_escape_once(text.strip)
-    escaped_text.sub(/\s([\w\.\?\!]+)$/, '&nbsp;\1').html_safe
+    escaped_text.sub(/\s([\w\.\?\!\:]+)$/, '&nbsp;\1').html_safe
   end
 
   def strip_trailing_colons(text)
