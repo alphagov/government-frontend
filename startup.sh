838de8440da6b63870b07b5993550af7a798c5cc
Tijmen Brommet <tijmen@gmail.com>
2017-11-01 17:45:24 +0000

    Set environment vars for Rummager
    Rummager/search is a dependency of the navigation helpers (it uses it
    to show related content). We normally don't notice this because the
    govuk_navigation_helpers gem is designed to fail gracefully when
    rummager isn't available.

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ if [[ $1 == "--live" ]] ; then
   GOVUK_WEBSITE_ROOT=https://www.gov.uk \
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
+  PLEK_SERVICE_RUMMAGER_URI=${PLEK_SERVICE_RUMMAGER_URI-https://www.gov.uk/api} \
   bundle exec rails s -p 3090
 else
   bundle exec rails s -p 3090
