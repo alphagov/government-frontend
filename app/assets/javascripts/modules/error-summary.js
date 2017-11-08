bd612b46676fd8ad9c4ded1a0b7a68d1b6fb1f1a
Nick Colley <nickcolley@gds0480.local>
2017-11-08 12:07:42 +0000

    Ensure error summary is focused on page load

---
+++ app/assets/javascripts/modules/error-summary.js
@@ -0,0 +1,8 @@
+// Will be removed after AB finishes
+
+$(window).load(function () {
+  // If there is an error summary, set focus to the summary
+  if ($('.js-error-summary').length) {
+    $('.js-error-summary').focus()
+  }
+})
