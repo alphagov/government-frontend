9ad72d62b8233357bd2555744416fa8da32b73ce
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-11-08 16:57:13 +0000

    Add default path values for hard-coded routes
    As the routes are hard-coded, they don't pass a value for `path`
    to the controller. This means that when government-fronted is run
    locally it can't load the content item. We can get around this by
    passing a default value for the path.

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   get 'healthcheck', to: proc { [200, {}, ['']] }
 
-  get 'log-in-file-self-assessment-tax-return/choose-sign-in', to: 'content_items#choose_sign_in', as: :choose_sign_in
-  get 'log-in-file-self-assessment-tax-return/not-registered', to: 'content_items#not_registered'
-  get 'log-in-file-self-assessment-tax-return/lost-account-details', to: 'content_items#lost_account_details', as: :lost_account_details
+  get 'log-in-file-self-assessment-tax-return/choose-sign-in', to: 'content_items#choose_sign_in', as: :choose_sign_in, defaults: { path: '/log-in-file-self-assessment-tax-return/choose-sign-in' }
+  get 'log-in-file-self-assessment-tax-return/not-registered', to: 'content_items#not_registered', defaults: { path: '/log-in-file-self-assessment-tax-return/not-registered' }
+  get 'log-in-file-self-assessment-tax-return/lost-account-details', to: 'content_items#lost_account_details', as: :lost_account_details, defaults: { path: '/log-in-file-self-assessment-tax-return/lost-account-details' }
   post 'log-in-file-self-assessment-tax-return/choose-sign-in', to: 'content_items#sign_in_options'
 
   get '*path/:variant' => 'content_items#show',
