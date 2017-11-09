78f5d2c29585ffffc0231f539518581c40714f8e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-11-09 13:56:23 +0000

    Move self assessment tests into their own file
    The content_items_controller_test file was becoming cluttered
    with tests that relate to the self assessment A/B test.
    
    Moving the tests to their own file will make them easier to 
    remove when the A/B test finishes. The `path_for` method has 
    been duplicated rather than being moved to a helper class for
    the same reason.

---
+++ test/controllers/self_assessment_ab_test_controller_test.rb
@@ -0,0 +1,122 @@
+require 'test_helper'
+
+class ContentItemsControllerTest < ActionController::TestCase
+  include GdsApi::TestHelpers::ContentStore
+  include GovukAbTesting::MinitestHelpers
+
+  test "shows the original page content on the control version of the self assessment guide" do
+    content_item = content_store_has_schema_example('guide', 'guide')
+    path = 'log-in-file-self-assessment-tax-return'
+    content_item['base_path'] = "/#{path}"
+    content_item['details'] = {
+      parts: [
+        body: "The original part one"
+      ]
+    }
+    content_store_has_item(content_item['base_path'], content_item)
+
+    setup_ab_variant("SelfAssessmentSigninTest", "A")
+
+    get :show, params: { path: path_for(content_item) }
+    assert @response.body.include?('The original part one')
+  end
+
+  test "overwrites content for the first page of the self assessment guide" do
+    content_item = content_store_has_schema_example('guide', 'guide')
+    path = 'log-in-file-self-assessment-tax-return'
+    content_item['base_path'] = "/#{path}"
+
+    content_store_has_item(content_item['base_path'], content_item)
+
+    setup_ab_variant("SelfAssessmentSigninTest", "B")
+
+    get :show, params: { path: path_for(content_item) }
+    assert @response.body.include?('Sign in to continue')
+  end
+
+  test "other guide pages are not overwritten" do
+    %w(A B).each do |variant|
+      content_item = content_store_has_schema_example('guide', 'guide')
+      path = 'guide-page'
+      content_item['base_path'] = "/#{path}"
+      content_item['details'] = {
+        parts: [
+          body: "The original part one"
+        ]
+      }
+
+      content_store_has_item(content_item['base_path'], content_item)
+
+      setup_ab_variant("SelfAssessmentSigninTest", variant)
+
+      get :show, params: { path: path_for(content_item) }
+      assert @response.body.include?('The original part one')
+    end
+  end
+
+  test "choose_sign_in" do
+    content_item = content_store_has_schema_example("guide", "guide")
+    content_item["base_path"] = "/log-in-file-self-assessment-tax-return/choose-sign-in"
+    content_store_has_item(content_item["base_path"], content_item)
+
+    setup_ab_variant("SelfAssessmentSigninTest", "B")
+
+    get :choose_sign_in, params: { path: path_for(content_item) }
+    assert_response 200
+    assert_not @response.body.include?("You haven't selected an option")
+    assert_template("content_items/signin/choose-sign-in")
+  end
+
+  test "choose_sign_in with error" do
+    content_item = content_store_has_schema_example("guide", "guide")
+    content_item["base_path"] = "/log-in-file-self-assessment-tax-return/choose-sign-in"
+    content_store_has_item(content_item["base_path"], content_item)
+
+    setup_ab_variant("SelfAssessmentSigninTest", "B")
+
+    get :choose_sign_in, params: { path: path_for(content_item), error: true }
+    assert_response 200
+    assert_template("content_items/signin/choose-sign-in")
+    assert @response.body.include?("You haven't selected an option")
+  end
+
+  test "sign_in_options with sign-in-option param set" do
+    post :sign_in_options, params: { "sign-in-option" => "government-gateway" }
+
+    assert_response :redirect
+    assert_redirected_to "https://www.tax.service.gov.uk/account"
+  end
+
+  test "sign_in_options with no sign-in-option param set" do
+    post :sign_in_options
+
+    assert_response :redirect
+    assert_redirected_to "#{choose_sign_in_path}?error=true"
+  end
+
+  test "lost_account_details" do
+    content_item = content_store_has_schema_example("guide", "guide")
+    content_item["base_path"] = "/log-in-file-self-assessment-tax-return/lost-account-details"
+    content_store_has_item(content_item["base_path"], content_item)
+
+    get :lost_account_details, params: { path: path_for(content_item) }
+    assert_response 200
+    assert_template("content_items/signin/lost-account-details")
+  end
+
+  test "not_registered" do
+    content_item = content_store_has_schema_example("guide", "guide")
+    content_item["base_path"] = "/log-in-file-self-assessment-tax-return/not-registered"
+    content_store_has_item(content_item["base_path"], content_item)
+
+    get :not_registered, params: { path: path_for(content_item) }
+    assert_response 200
+    assert_template("content_items/signin/not-registered")
+  end
+
+  def path_for(content_item, locale = nil)
+    base_path = content_item['base_path'].sub(/^\//, '')
+    base_path.gsub!(/\.#{locale}$/, '') if locale
+    base_path
+  end
+end
