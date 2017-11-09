c7d28e5ed5719ca1f5540cdfee1c07dd62e8822b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-11-09 16:02:10 +0000

    Refactor Self Assessment A/B test setup
    The set up for the Self Assessment A/B test has been moved into
    a concern.
    
    Benefits to this are:
    1. The set up doesn't need to added to any new controller
    methods
    
    2. It will make it easier to remove when the A/B test has
    completed.

---
+++ app/controllers/concerns/self_assessment_ab_testable.rb
@@ -0,0 +1,48 @@
+module SelfAssessmentABTestable
+  SELF_ASSESSMENT_AB_PAGES = %w(
+    /log-in-file-self-assessment-tax-return
+    /log-in-file-self-assessment-tax-return/choose-sign-in
+    /log-in-file-self-assessment-tax-return/not-registered
+    /log-in-file-self-assessment-tax-return/lost-account-details
+  ).freeze
+
+  def self.included(base)
+    base.helper_method(
+      :self_assessment_start_page?
+    )
+  end
+
+  def set_up_self_assessment_ab_test
+    ab_test = GovukAbTesting::AbTest.new("SelfAssessmentSigninTest", dimension: 65)
+
+    @self_assessment_requested_variant = ab_test.requested_variant(request.headers)
+    @self_assessment_requested_variant.configure_response(response)
+  end
+
+  def set_up_self_assessment_ab_content_item
+    set_up_self_assessment_ab_test
+    content_item = Services.content_store.content_item(content_item_path)
+    ContentItemPresenter.new(content_item, content_item_path)
+  end
+
+  def self_assessment_start_page?(content_item)
+    SELF_ASSESSMENT_AB_PAGES.include?(content_item["base_path"])
+  end
+
+  def replace_self_assessment_part_one(content_item)
+    if self_assessment_start_page?(content_item) && @self_assessment_requested_variant.variant?('B')
+      b_variant_content = File.read(Rails.root.join("app", "assets", "html", "self_assessment_b_variant.html"))
+      content_item["details"]["parts"].first["body"] = b_variant_content.to_s
+    end
+    content_item
+  end
+
+  def content_item_path
+    path_and_optional_locale = params
+                                 .values_at(:path, :locale)
+                                 .compact
+                                 .join('.')
+
+    '/' + URI.encode(path_and_optional_locale)
+  end
+end
--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -4,6 +4,7 @@ class ContentItemsController < ApplicationController
   include TasklistHeaderABTestable
   include TasklistABTestable
   include ContentNavigationABTestable
+  include SelfAssessmentABTestable
 
   rescue_from GdsApi::HTTPForbidden, with: :error_403
   rescue_from GdsApi::HTTPNotFound, with: :error_notfound
@@ -13,7 +14,6 @@ class ContentItemsController < ApplicationController
   rescue_from SpecialRouteReturned, with: :error_notfound
 
   attr_accessor :content_item
-  helper_method :self_assessment_start_page?
 
   def show
     set_up_self_assessment_ab_test
@@ -60,16 +60,6 @@ class ContentItemsController < ApplicationController
     else
       redirect_to choose_sign_in_path error: true
     end
-  end
-
-  def self_assessment_start_page?(content_item)
-    self_assessment_pages = %w(
-      /log-in-file-self-assessment-tax-return
-      /log-in-file-self-assessment-tax-return/choose-sign-in
-      /log-in-file-self-assessment-tax-return/not-registered
-      /log-in-file-self-assessment-tax-return/lost-account-details
-    )
-    self_assessment_pages.include?(content_item["base_path"])
   end
 
 private
@@ -137,27 +127,6 @@ private
     max_age = @content_item.content_item.cache_control.max_age
     cache_private = @content_item.content_item.cache_control.private?
     expires_in(max_age, public: !cache_private)
-  end
-
-  def set_up_self_assessment_ab_test
-    ab_test = GovukAbTesting::AbTest.new("SelfAssessmentSigninTest", dimension: 65)
-
-    @self_assessment_requested_variant = ab_test.requested_variant(request.headers)
-    @self_assessment_requested_variant.configure_response(response)
-  end
-
-  def replace_self_assessment_part_one(content_item)
-    if self_assessment_start_page?(content_item) && @self_assessment_requested_variant.variant?('B')
-      b_variant_content = File.read(Rails.root.join("app", "assets", "html", "self_assessment_b_variant.html"))
-      content_item["details"]["parts"].first["body"] = b_variant_content.to_s
-    end
-    content_item
-  end
-
-  def set_up_self_assessment_ab_content_item
-    set_up_self_assessment_ab_test
-    content_item = Services.content_store.content_item(content_item_path)
-    ContentItemPresenter.new(content_item, content_item_path)
   end
 
   def set_up_navigation
