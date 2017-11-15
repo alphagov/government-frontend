0b7efafc252e45d9652dab1d64873d92d949af10
Paul Hayes <fofr@users.noreply.github.com>
2017-11-15 09:27:09 +0000

    Allow content metadata to render any metadata types
    * Use pattern from static metadata component:
    https://govuk-static.herokuapp.com/component-guide/metadata
    * Allow further metadata to be added easily as design is iterated
    * This allows us to easily add national applicability

--- test/components/content_metadata_test.rb
+++ test/components/content_metadata_test.rb
@@ -9,22 +9,27 @@ class ContentMetadataTest < ComponentTestCase
     assert_empty render_component({})
   end
 
+  test "does not render when an 'other' object is provided without any values" do
+    assert_empty render_component(other: { From: [] })
+    assert_empty render_component(other: { a: false, b: "", c: [], d: {}, e: nil })
+  end
+
   test "renders a from link when from data is given" do
-    render_component(from: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>"])
-    assert_select ".app-c-content-metadata__from a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
-    assert_select ".app-c-content-metadata__from", text: 'From: Ministry of Defence'
+    render_component(other: { From: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>"] })
+    assert_select ".app-c-content-metadata__other a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
+    assert_select ".app-c-content-metadata__other", text: 'From: Ministry of Defence'
   end
 
 
   test "renders two from links when two publishers are given" do
-    render_component(from: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>", "<a href='/government/organisations/education-funding-agency'>Education Funding Agency</a>"])
-    assert_select ".app-c-content-metadata__from a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
-    assert_select ".app-c-content-metadata__from a[href='/government/organisations/education-funding-agency']", text: 'Education Funding Agency'
+    render_component(other: { From: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>", "<a href='/government/organisations/education-funding-agency'>Education Funding Agency</a>"] })
+    assert_select ".app-c-content-metadata__other a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
+    assert_select ".app-c-content-metadata__other a[href='/government/organisations/education-funding-agency']", text: 'Education Funding Agency'
   end
 
   test "renders a sentence when multiple publishers are given" do
-    render_component(from: ["<a href='/government/organisations/department-for-education'>Department for Education</a>", "<a href='/government/organisations/education-funding-agency'>Education Funding Agency</a>"])
-    assert_select ".app-c-content-metadata__from", text: 'From: Department for Education and Education Funding Agency'
+    render_component(other: { From: ["<a href='/government/organisations/department-for-education'>Department for Education</a>", "<a href='/government/organisations/education-funding-agency'>Education Funding Agency</a>"] })
+    assert_select ".app-c-content-metadata__other", text: 'From: Department for Education and Education Funding Agency'
   end
 
   test "renders published dates component when only published date is given" do
@@ -38,9 +43,9 @@ class ContentMetadataTest < ComponentTestCase
   end
 
   test "renders full metadata component when all parameters are given" do
-    render_component(from: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>"], published: "31 July 2017", last_updated: "20 September 2017", link_to_history: true)
+    render_component(other: { From: ["<a href='/government/organisations/ministry-of-defence'>Ministry of Defence</a>"] }, published: "31 July 2017", last_updated: "20 September 2017", link_to_history: true)
     assert_select ".app-c-published-dates"
-    assert_select ".app-c-content-metadata__from a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
-    assert_select ".app-c-content-metadata__from", text: 'From: Ministry of Defence'
+    assert_select ".app-c-content-metadata__other a[href='/government/organisations/ministry-of-defence']", text: 'Ministry of Defence'
+    assert_select ".app-c-content-metadata__other", text: 'From: Ministry of Defence'
   end
 end
