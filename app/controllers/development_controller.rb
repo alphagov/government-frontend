29e100d7377c5727bb1baab174ff4feb27bb0e2d
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-11-30 12:20:03 +0000

    Setup service sign in format boilerplate
    Adds only the choose sign in files for now.
    
    Rails looks in "app/views" for a template that matchs the format
    type. As that is to a one-to-one relationship for the service_sign_in
    format, we have created a base template ("service_sign_in"), that will
    then call a partial template, based on the page that the user is on.

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -5,7 +5,7 @@ class DevelopmentController < ApplicationController
     @schema_names = %w[answer case_study coming_soon consultation contact
                        corporate_information_page detailed_guide document_collection
                        fatality_notice guide help_page html_publication news_article
-                       placeholder_corporate_information_page publication specialist_document
+                       placeholder_corporate_information_page publication service_sign_in specialist_document
                        speech statistical_data_set statistics_announcement take_part
                        topical_event_about_page travel_advice working_group
                        world_location_news_article]
---
+++ test/integration/service_sign_in/choose_sign_in_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module ServiceSignIn
+  class ChooseSignInTest < ActionDispatch::IntegrationTest
+  end
+end
