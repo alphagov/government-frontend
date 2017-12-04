ff1d66d5052b70fd0c2073077c78c43bb15faccb
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-12-04 11:36:38 +0000

    Add error-message styles, templates, tests, documentation
    Generate error summary component using bundle exec rails generate govuk_publishing_components:component error-message
    
    Forked from GOV.UK Frontend

---
+++ test/components/error_message_test.rb
@@ -0,0 +1,23 @@
+require 'component_test_helper'
+
+class ErrorMessageTest < ComponentTestCase
+  def component_name
+    "error-message"
+  end
+
+  test "fails to render no data is given" do
+    assert_raise do
+      render_component({})
+    end
+  end
+
+  test "renders an error message correctly" do
+    render_component(text: 'Descriptive error message')
+    assert_select ".app-c-error-message", text: 'Descriptive error message'
+  end
+
+  test "renders an error message with an id" do
+    render_component(text: 'Descriptive error message with id', id: 'unique-error-id')
+    assert_select ".app-c-error-message[id='unique-error-id']", text: 'Descriptive error message with id'
+  end
+end
