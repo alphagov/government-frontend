26cbc72e4eba1b72ced32ae9f582f7572a18289d
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-12-11 18:27:30 +0000

    Add title to create_new_account template

--- app/presenters/service_sign_in/create_new_account_presenter.rb
+++ app/presenters/service_sign_in/create_new_account_presenter.rb
@@ -3,5 +3,15 @@ module ServiceSignIn
     def page_type
       "create_new_account"
     end
+
+    def title
+      create_new_account["title"]
+    end
+
+  private
+
+    def create_new_account
+      content_item["details"]["create_new_account"]
+    end
   end
 end
