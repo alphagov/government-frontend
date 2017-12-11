fbba670327c75cd39855e8ff632cc242bc3e9698
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-12-11 11:52:45 +0000

    Add title to choose_sign_in template

--- app/presenters/service_sign_in/choose_sign_in_presenter.rb
+++ app/presenters/service_sign_in/choose_sign_in_presenter.rb
@@ -3,5 +3,15 @@ module ServiceSignIn
     def page_type
       "choose_sign_in"
     end
+
+    def title
+      choose_sign_in["title"]
+    end
+
+  private
+
+    def choose_sign_in
+      content_item["details"]["choose_sign_in"]
+    end
   end
 end
