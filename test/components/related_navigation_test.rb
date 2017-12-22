eefddc41f5b5cca0c7640019fbcc3947c423d249
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2017-12-22 10:16:42 +0000

    Fix number skipping in section_index

--- test/components/related_navigation_test.rb
+++ test/components/related_navigation_test.rb
@@ -238,7 +238,7 @@ class RelatedNavigationTest < ComponentTestCase
 
     assert_select ".app-c-related-navigation__nav-section ul[data-module='track-click']"
     assert_select ".app-c-related-navigation__section-link[data-track-category='relatedLinkClicked']"
-    assert_select ".app-c-related-navigation__section-link[data-track-action='2.1 Explore the topic']"
+    assert_select ".app-c-related-navigation__section-link[data-track-action='1.1 Explore the topic']"
     assert_select ".app-c-related-navigation__section-link[data-track-label='/apprenticeships']"
   end
 end
