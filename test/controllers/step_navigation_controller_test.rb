34556bc878287dd8a3be8a02ebe8dc1a02683b0c
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-02-02 14:38:38 +0000

    Updates for navigation_helpers/pub_components changes
    govuk_navigation_helpers no longer contains AB testing logic for enabling/
    disabling step by step navigation.  It also has a change to the format of the
    data to pass to the task_list component.
    
    govuk_publishing_components has the corresponding change to the format of the
    data that task_list components expect, but also has renamed task_list elements
    to step_by_step_nav.
    
    We need to update this code to work with that.  It makes for an unwieldy review
    but nothing will work if we do piecemeal commits. :-/

---
+++ test/controllers/step_navigation_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ContentItemsControllerTest < ActionController::TestCase
+  include GdsApi::TestHelpers::ContentStore
+  include GovukAbTesting::MinitestHelpers
+
+  %w(guide answer publication).each do |schema_name|
+    test "#{schema_name} shows step by step navigation where relevant" do
+      content_item = content_store_has_schema_example(schema_name, schema_name)
+      content_item['base_path'] = "/pass-plus"
+      path = content_item['base_path'][1..-1]
+
+      content_store_has_item(content_item['base_path'], content_item)
+
+      get :show, params: { path: path }
+
+      assert_response 200
+      assert response.body.include?("Learn to drive a car: step by step")
+    end
+
+    test "#{schema_name} does not show step by step navigation where relevant" do
+      content_item = content_store_has_schema_example(schema_name, schema_name)
+      content_item['base_path'] = "/not-part-of-a-step-by-step"
+      path = content_item['base_path'][1..-1]
+
+      content_store_has_item(content_item['base_path'], content_item)
+
+      get :show, params: { path: path }
+
+      assert_response 200
+      refute response.body.include?("Learn to drive a car: step by step")
+    end
+  end
+end
