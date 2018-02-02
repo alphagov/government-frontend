c6979d310d878babbf8c8cb1cd124759ae695bd4
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-02-02 16:28:40 +0000

    Remove other logging setup
    This is [already in govuk_app_config](https://github.com/alphagov/govuk_app_config/blob/5220b89da3be24429656e69f0c0f90bef11abdd4/lib/govuk_app_config/govuk_logging.rb#L15-L16)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,16 +70,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new($stderr))
-
-  $real_stdout = $stdout.clone
-  $stdout.reopen($stderr)
-
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 end
