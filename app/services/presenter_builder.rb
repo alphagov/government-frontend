7bce4937cba0c3deee971e228451f0019ac9de7e
Tijmen Brommet <tijmen@gmail.com>
2018-02-09 15:48:34 +0000

    Extract methods to build the presenter
    There are a lot of methods in the ContentItemsController that relate to
    the process of determining the correct presenter. This moves them into
    a single class.

---
+++ app/services/presenter_builder.rb
@@ -0,0 +1,57 @@
+class PresenterBuilder
+  attr_reader :content_item, :content_item_path
+
+  def initialize(content_item, content_item_path)
+    @content_item = content_item
+    @content_item_path = content_item_path
+  end
+
+  def presenter
+    raise SpecialRouteReturned if special_route?
+    raise RedirectRouteReturned, content_item if redirect_route?
+
+    presenter_name.constantize.new(content_item, content_item_path)
+  end
+
+private
+
+  def special_route?
+    content_item && content_item["document_type"] == "special_route"
+  end
+
+  def redirect_route?
+    content_item && content_item["schema_name"] == "redirect"
+  end
+
+  def presenter_name
+    if service_sign_in_format?
+      return service_sign_in_presenter_name
+    end
+
+    content_item["schema_name"].classify + "Presenter"
+  end
+
+  def service_sign_in_format?
+    content_item["schema_name"] == "service_sign_in"
+  end
+
+  def service_sign_in_presenter_name
+    slug = content_item_path.split("/").last
+
+    if content_item.dig("details", "create_new_account", "slug") == slug
+      return "ServiceSignIn::CreateNewAccountPresenter"
+    end
+
+    "ServiceSignIn::ChooseSignInPresenter"
+  end
+
+  class RedirectRouteReturned < StandardError
+    attr_reader :content_item
+
+    def initialize(content_item)
+      super("Redirect content_item detected")
+      @content_item = content_item
+    end
+  end
+  class SpecialRouteReturned < StandardError; end
+end
