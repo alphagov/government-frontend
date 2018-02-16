e649d6c01a49e8145d429915d33155735d3943cc
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-02-16 21:42:38 +0000

    Revert using foreman in dev environment
    This removes foreman from the Gemfile and from the startup.sh usage and
    instead installs foreman separately via `gem install` in the Dockerfile
    so that it can be used in the docker environment.
    
    The reason for this is that running everything via unicorn in dev can
    cause some wtfs (such as breaking better_errors) and installing foreman
    via Gemfile does contradict some contenious advice given by the foreman
    gem maintainer.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ruby:2.4.2
 RUN apt-get update -qq && apt-get upgrade -y
-
 RUN apt-get install -y build-essential nodejs && apt-get clean
+RUN gem install foreman
 
 ENV GOVUK_APP_NAME government-frontend
 ENV GOVUK_ASSET_ROOT http://assets-origin.dev.gov.uk
@@ -22,4 +22,4 @@ RUN GOVUK_WEBSITE_ROOT=https://www.gov.uk GOVUK_APP_DOMAIN=www.gov.uk RAILS_ENV=
 
 HEALTHCHECK CMD curl --silent --fail localhost:$PORT || exit 1
 
-CMD bundle exec foreman run web
+CMD foreman run web
--- startup.sh
+++ startup.sh
@@ -8,14 +8,14 @@ if [[ $1 == "--live" ]] ; then
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
   PLEK_SERVICE_RUMMAGER_URI=${PLEK_SERVICE_RUMMAGER_URI-https://www.gov.uk/api} \
-  bundle exec foreman run web
+  bundle exec rails s -p 3090
 elif [[ $1 == "--dummy" ]] ; then
   GOVUK_APP_DOMAIN=www.gov.uk \
   GOVUK_WEBSITE_ROOT=https://www.gov.uk \
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://govuk-content-store-examples.herokuapp.com/api} \
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
   PLEK_SERVICE_RUMMAGER_URI=${PLEK_SERVICE_RUMMAGER_URI-https://www.gov.uk/api} \
-  bundle exec foreman run web
+  bundle exec rails s -p 3090
 else
-  bundle exec foreman run web
+  bundle exec rails s -p 3090
 fi
