cbd8b53e04c0b00753a4eae97106017f9df30102
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2018-02-19 11:45:28 +0000

    Amend finder link text for publications

--- app/presenters/navigation/publications.rb
+++ app/presenters/navigation/publications.rb
@@ -2,11 +2,7 @@ module Navigation
   module Publications
     include Finders
 
-    UPPERCASE_DOCTYPES = %w(
-      foi_release
-      national_statistics
-      official_statistics
-    ).freeze
+    STATISTICS_DOCTYPES = %w(national_statistics official_statistics).freeze
 
     def finder_path_and_params
       "/government/publications?#{facet_params}"
@@ -27,8 +23,11 @@ module Navigation
     end
 
     def pluralised_document_type
+      return "Statistics" if STATISTICS_DOCTYPES.include?(document_type)
+      return "FOI releases" if document_type == "foi_release"
+
       doctype = I18n.t("content_item.schema_name.#{document_type}", count: 2, locale: :en)
-      doctype[0] = doctype[0].downcase unless UPPERCASE_DOCTYPES.include?(document_type)
+      doctype[0] = doctype[0].downcase
       doctype
     end
   end
--- test/presenters/navigation/publications_test.rb
+++ test/presenters/navigation/publications_test.rb
@@ -54,12 +54,29 @@ class PublicationsTest < PresenterTestCase
     assert_equal("FOI releases", pluralised_document_type)
 
     self.stubs(:document_type).returns("national_statistics")
-    assert_equal("National Statistics", pluralised_document_type)
+    assert_equal("Statistics", pluralised_document_type)
 
     self.stubs(:document_type).returns("official_statistics")
-    assert_equal("Official Statistics", pluralised_document_type)
+    assert_equal("Statistics", pluralised_document_type)
 
     self.stubs(:document_type).returns("press_release")
     assert_equal("press releases", pluralised_document_type)
+  end
+
+  test "finder link text" do
+    self.stubs(:document_type).returns("publication")
+    stub_content_item
+
+    assert_equal "More publications about borders and immigation from UK Visas and Immigration", link_text
+  end
+
+  test "finder link text with policy area capitalisation preserved" do
+    self.stubs(:document_type).returns("publication")
+    stub_content_item("links" => {
+      "primary_publishing_organisation" => [{ "title" => "Department of Health and Social Care" }],
+      "policy_areas" => [{ "title" => "National Health Service" }]
+    })
+
+    assert_equal "More publications about National Health Service from Department of Health and Social Care", link_text
   end
 end
