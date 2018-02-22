66cbb5d32e8be7ba8b1ecd5ba76f7204333a71d1
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2018-02-22 13:18:51 +0000

    Disable caching of content-store responses
    By default, gds-api-adapters caches responses based on the `max-age` set
    in the response's cache control headers. This means that
    government-frontend can use a very stale max-age when setting cache
    headers on its own responses.
    
    In the worst case scenario, this means that content changes can take up
    to 30 minutes to appear on the site.
    
    Disabling this cache should not put much more load on the content store
    because responses are cached so heavily in Varnish and the CDN, and
    these handle cache headers correctly.

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -3,7 +3,13 @@ require 'gds_api/rummager'
 
 module Services
   def self.content_store
-    @content_store ||= GdsApi::ContentStore.new(Plek.current.find("content-store"))
+    @content_store ||= GdsApi::ContentStore.new(
+      Plek.current.find("content-store"),
+      # Disable caching to avoid caching a stale max-age in the cache control
+      # headers, which would cause this app to set the wrong max-age on its
+      # own responses
+      disable_cache: true,
+    )
   end
 
   def self.rummager
