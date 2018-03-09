539f7048a6829571f9445096e5871c28d92c5002
Callum Knights <callumknights@gds5084.local>
2018-03-09 14:42:02 +0000

    HUB-73: Added tests for the new service sign in page for personal tax account service.
    Added tests to check the correct URL's are being presented for both gateway and verify personal tax account
    sign in, as well as a test to check the correct service text is presented for personal tax account.
    
    Co-authored-by: Simon Wilson <simon.wilson@digital.cabinet-office.gov.uk>

--- test/presenters/service_sign_in/verify_hub_trial_create_new_account_presenter_test.rb
+++ test/presenters/service_sign_in/verify_hub_trial_create_new_account_presenter_test.rb
@@ -4,11 +4,14 @@ class ServiceSignInPresenterTest
   class VerifyHubTrialCreateNewAccount < ActiveSupport::TestCase
     GOV_GATE_URL_FOR_COMPANY_CAR = "https://www.tax.service.gov.uk/paye/company-car/start-government-gateway".freeze
     GOV_GATE_URL_FOR_INCOME_TAX = "https://www.tax.service.gov.uk/check-income-tax/start-government-gateway?_ga=2.114080007.145612230.1512381177-373904926.147".freeze
+    GOV_GATE_URL_FOR_PERSONAL_TAX = "https://www.tax.service.gov.uk/personal-account/start-government-gateway".freeze
     VERIFY_URL_FOR_COMPANY_CAR = "https://www.tax.service.gov.uk/paye/company-car/start-verify".freeze
     VERIFY_URL_FOR_INCOME_TAX = "https://www.tax.service.gov.uk/check-income-tax/start-verify?_ga=2.114080007.145612230.1512381177-373904926.1473694521".freeze
+    VERIFY_URL_FOR_PERSONAL_TAX = "https://www.tax.service.gov.uk/personal-account/start-verify".freeze
 
     SERVICE_TEXT_FOR_COMPANY_CAR = "check or update your company car tax".freeze
     SERVICE_TEXT_FOR_INCOME_TAX = "check your income tax for the current year".freeze
+    SERVICE_TEXT_FOR_PERSONAL_TAX = "access your personal tax account".freeze
 
     def schema_name
       "service_sign_in"
@@ -55,14 +58,29 @@ class ServiceSignInPresenterTest
       assert_equal VERIFY_URL_FOR_INCOME_TAX, @presented_item.verify_url
     end
 
+    test 'presents correct url for gateway sign up - personal tax account' do
+      schema_item['base_path'] = "personal-tax-account/sign-in"
+      assert_equal GOV_GATE_URL_FOR_PERSONAL_TAX, @presented_item.govenment_gateway_url
+    end
+
+    test 'presents correct url for verify sign up - personal tax account' do
+      schema_item['base_path'] = "personal-tax-account/sign-in"
+      assert_equal VERIFY_URL_FOR_PERSONAL_TAX, @presented_item.verify_url
+    end
+
     test 'presents correct service text - update company car details' do
       schema_item['base_path'] = "update-company-car-details/sign-in"
       assert_equal SERVICE_TEXT_FOR_COMPANY_CAR, @presented_item.service_specific_text
     end
 
-    test 'presents correct service text  - check income tax' do
+    test 'presents correct service text - check income tax' do
       schema_item['base_path'] = "check-income-tax-current-year/sign-in"
       assert_equal SERVICE_TEXT_FOR_INCOME_TAX, @presented_item.service_specific_text
+    end
+
+    test 'presents correct service text - personal tax account' do
+      schema_item['base_path'] = "personal-tax-account/sign-in"
+      assert_equal SERVICE_TEXT_FOR_PERSONAL_TAX, @presented_item.service_specific_text
     end
 
     test 'presents the back_link' do
