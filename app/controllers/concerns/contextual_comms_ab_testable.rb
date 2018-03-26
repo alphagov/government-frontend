571b29e970c538739a2bdb1dc443604b8f4cb84d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-03-26 13:13:36 +0000

    Add basic AB test setup to Contextual Comms AB Testable

--- app/controllers/concerns/contextual_comms_ab_testable.rb
+++ app/controllers/concerns/contextual_comms_ab_testable.rb
@@ -1,2 +1,28 @@
 module ContextualCommsAbTestable
+  GOOGLE_ANALYTICS_CUSTOM_DIMENSION = 70
+
+  def self.included(base)
+    base.helper_method(
+      :contextual_comms_test_variant,
+    )
+    base.after_action :set_test_response_header
+  end
+
+  def contextual_comms_test
+    @contextual_comms_test ||= GovukAbTesting::AbTest.new(
+      "ContextualComms",
+      dimension: GOOGLE_ANALYTICS_CUSTOM_DIMENSION,
+      allowed_variants: %w(NoCampaign BlueBoxCampaign NativeCampaign),
+      control_variant: "NoCampaign"
+    )
+  end
+
+  def contextual_comms_test_variant
+    @contextual_comms_test_variant ||= contextual_comms_test.requested_variant(request.headers)
+  end
+
+  def set_test_response_header
+    contextual_comms_test_variant.configure_response(response)
+  end
+  end
 end
