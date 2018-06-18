2c18604d4b922439ed98e5576842a20b54cc16cc
Tijmen Brommet <tijmen@gmail.com>
2018-06-18 16:55:35 +0100

    Remove support for Static-based components
    "Wop wop"
    
    https://trello.com/c/pU7YINt1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Slimmer::GovukComponents
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery except: :service_sign_in_options
