9ee5f80c23e7405dcf5cd6c53d3218c70087faa6
Rachel Smith <rachelcodesmith@outlook.com>
2018-06-19 11:51:02 +0100

    HUB-97: Log choice of verify users
    We'd like to see what choices exisiting Verify users make on the
    service sign-in page. Therefore we're calling a Verify endpoint using
    an ajax call and based on the user cookie to decide whether it's a
    Verify user and report accordingly. This is a baseline test to learn about the users' behaviour.
    
    Co-Authored-by: Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>

--- app/assets/javascripts/modules/track-radio-group.js
+++ app/assets/javascripts/modules/track-radio-group.js
@@ -8,21 +8,45 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   GOVUK.Modules.TrackRadioGroup = function () {
     this.start = function (element) {
-      element.on('submit', function (event) {
+      track(element)
+
+      checkVerifyUser(element)
+    }
+
+    function track (element, withHint) {
+        element.on('submit', function (event) {
+        
         var options = { transport: 'beacon' }
 
         var $submittedForm = $(event.target)
 
         var $checkedOption = $submittedForm.find('input:checked')
 
-        var checkedValue = $checkedOption.val()
+        var checkedValue = $checkedOption.val();
 
         if (typeof checkedValue === 'undefined') {
           checkedValue = 'submitted-without-choosing'
         }
-
-        GOVUK.analytics.trackEvent('Radio button chosen', checkedValue, options)
+        GOVUK.analytics.trackEvent('Radio button chosen', checkedValue + (withHint ? '-with-hint' : ''), options)
       })
     }
+
+    function checkVerifyUser (element) {
+      $.ajax({
+        url: 'https://www.signin.service.gov.uk/hint', 
+        cache: false,
+        dataType: 'jsonp',
+        timeout: 3000
+      }).then(function(data){
+          GOVUK.Modules.TrackRadioGroup.trackVerifyUser(element, data);
+      }, function(e){console.log("error", e)})
+    }
+
+    this.trackVerifyUser = function (element, data) {
+        if (data != null && data.value === true) {
+            GOVUK.analytics.trackEvent('verify-hint', 'shown', { transport: 'beacon'});
+            track(element, data.value);
+      }
+    }
   }
-})(window, window.GOVUK)
+})(window, window.GOVUK);
