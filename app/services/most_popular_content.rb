55a81c8a15c54b725ddd0269bdff4d3045657a7c
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-07-20 15:42:12 +0100

    Allow multiple taxons
    Where content is tagged to more than one taxon, get the tagged content for all those taxons.
    
    Note that this doesn't put any limit on the number of taxons allowed.  We may
    want to set an arbitrary limit on this just to be on the safe side.

--- app/services/most_popular_content.rb
+++ app/services/most_popular_content.rb
@@ -3,16 +3,16 @@ require 'gds_api/rummager'
 class MostPopularContent
   include RummagerFields
 
-  attr_reader :content_id, :filter_content_purpose_supergroup, :number_of_links
+  attr_reader :content_ids, :filter_content_purpose_supergroup, :number_of_links
 
-  def initialize(content_id:, filter_content_purpose_supergroup:, number_of_links: 5)
-    @content_id = content_id
+  def initialize(content_ids:, filter_content_purpose_supergroup:, number_of_links: 5)
+    @content_ids = content_ids
     @filter_content_purpose_supergroup = filter_content_purpose_supergroup
     @number_of_links = number_of_links
   end
 
-  def self.fetch(content_id:, filter_content_purpose_supergroup:)
-    new(content_id: content_id, filter_content_purpose_supergroup: filter_content_purpose_supergroup).fetch
+  def self.fetch(content_ids:, filter_content_purpose_supergroup:)
+    new(content_ids: content_ids, filter_content_purpose_supergroup: filter_content_purpose_supergroup).fetch
   end
 
   def fetch
@@ -26,7 +26,7 @@ private
       start: 0,
       count: number_of_links,
       fields: RummagerFields::TAXON_SEARCH_FIELDS,
-      filter_part_of_taxonomy_tree: Array(content_id),
+      filter_part_of_taxonomy_tree: content_ids,
       order: '-popularity',
     }
     params[:filter_content_purpose_supergroup] = filter_content_purpose_supergroup if filter_content_purpose_supergroup.present?
