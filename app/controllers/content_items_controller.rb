d6e4b13c03f65c7351020a1b30fb17a6b972dc54
Mahmud Hussain <mahmudhussain@gds7463.local>
2018-07-24 09:38:45 +0100

    Add most recent news and communications
    - Retrieves most recent content for News and Communications supergroup

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -69,9 +69,11 @@ private
 
       taxon_ids = taxons.map { |taxon| taxon["content_id"] }
       services = Supergroups::Services.new(taxon_ids)
+      news_and_communications = Supergroups::NewsAndCommunications.new(taxon_ids)
 
       @taxonomy_navigation = {
         services: (services.all_services if services.any_services?),
+        news_and_communications: news_and_communications.tagged_content,
       }
 
       @tagged_taxons = taxons.map do |taxon|
