b3a791d308aa8f65f2577b5da17e52c5567d7f16
Andrew Garner <andrew@andrewgarner.com>
2018-07-30 16:32:18 +0100

    Remove related collections from the sidebar
    No longer display related collections in the sidebar if the new
    navigation is enabled.

---
+++ test/integration/content_pages_related_navigation_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ContentPagesRelatedNavigationTest < ActionDispatch::IntegrationTest
+  include ContentPagesNavTestHelper
+  include GdsApi::TestHelpers::Rummager
+
+  test "ContentPagesNav variant A shows related collections in the sidebar" do
+    setup_variant_a
+
+    setup_and_visit_content_item('case_study')
+
+    assert page.has_css?('.gem-c-related-navigation__sub-heading', text: 'Collection')
+  end
+
+  test "ContentPagesNav variant B does not show related collections in the sidebar" do
+    setup_variant_b
+
+    setup_and_visit_content_item('case_study')
+
+    refute page.has_css?('.gem-c-related-navigation__sub-heading', text: 'Collection')
+  end
+
+  def setup_variant_a
+    ContentItemsController.any_instance.stubs(:show_new_navigation?).returns(false)
+  end
+
+  def setup_variant_b
+    ContentItemsController.any_instance.stubs(:show_new_navigation?).returns(true)
+  end
+
+  def schema_type
+    "case_study"
+  end
+end
