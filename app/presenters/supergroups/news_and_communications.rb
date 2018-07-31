4e661170ef513cd92ea463e5debfd3c5b19cac53
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-07-31 08:36:27 +0100

    Add news and communications section
    News and comms are different from the other sections in that we show the image
    from the article if it's a featured link (and the image is present).
    
    This will be updated in future to get content images from rummager which
    removes the need to call content store for each item.
    
    We'll also update this to display results as one list rather than separate
    items.

---
+++ app/presenters/supergroups/news_and_communications.rb
@@ -0,0 +1,75 @@
+module Supergroups
+  class NewsAndCommunications < Supergroup
+    attr_reader :content
+
+    def initialize(current_path, taxon_ids)
+      @taxon_ids = taxon_ids
+      @current_path = current_path
+      @content = fetch
+    end
+
+    def all_news
+      {
+        documents: tagged_content,
+        promoted_content: promoted_content,
+      }
+    end
+
+    def any_news?
+      @content.any?
+    end
+
+    def tagged_content
+      items = @content.drop(promoted_content_count)
+      format_document_data(items)
+    end
+
+    def promoted_content
+      items = @content.shift(promoted_content_count)
+      content = format_document_data(items, data_category: "ImageCardClicked")
+
+      content.each do |document|
+        document_image = news_item_photo(document[:link][:path])
+        document[:image] = {
+          url: document_image["url"],
+          alt: document_image["alt_text"],
+          context: document_image["context"]
+        }
+      end
+
+      content
+    end
+
+  private
+
+    def fetch
+      return [] if @taxon_ids.empty?
+
+      MostRecentContent.fetch(
+        content_ids: @taxon_ids,
+        current_path: @current_path,
+        filter_content_purpose_supergroup: "news_and_communications"
+      )
+    end
+
+    def promoted_content_count
+      3
+    end
+
+    def news_item_photo(base_path)
+      default_news_image = {
+        "url" => "https://assets.publishing.service.gov.uk/government/assets/placeholder.jpg",
+        "alt_text" => ""
+      }
+
+      news_item = ::Services.content_store.content_item(base_path).to_h
+
+      image = news_item["details"]["image"] || default_news_image
+      date = Date.parse(news_item["public_updated_at"]).strftime("%d %B %Y")
+      document_type = news_item["document_type"].humanize
+      image["context"] = "#{document_type} - #{date}"
+
+      image
+    end
+  end
+end
