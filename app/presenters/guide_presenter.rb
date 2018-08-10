749e6a354159a430107605b41e4b033deb9538c0
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-08-10 14:15:11 +0000

    Use hide_chapter_navigation on guides
    - only happens if hide_chapter_navigation is true and the guide is in a step by step
    - promotes guide part subtitle to be the page title
    - hides guide contents list
    - hides next/prev navigation and guide print link

--- app/presenters/guide_presenter.rb
+++ app/presenters/guide_presenter.rb
@@ -41,9 +41,30 @@ class GuidePresenter < ContentItemPresenter
     end
   end
 
+  def show_guide_navigation?
+    multi_page_guide? && !hide_chapter_navigation?
+  end
+
+  def content_title
+    if parts.any?
+      return current_part_title if hide_chapter_navigation?
+    end
+
+    title
+  end
+
 private
 
   def draft_access_token_param
     "token=#{draft_access_token}" if draft_access_token
+  end
+
+  def hide_chapter_navigation?
+    hide_navigation_elements = content_item.parsed_content["details"]["hide_chapter_navigation"]
+    part_of_step_navs? && hide_navigation_elements
+  end
+
+  def part_of_step_navs?
+    content_item.parsed_content["links"]["part_of_step_navs"].present?
   end
 end
