9025ac66ff7c2836df766765eb0c8a8d57d4d72c
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-12-05 11:26:24 +0000

    Add News Image module for presenters
    This extracts some duplicated logic for deciding which image to display on a
    page.

---
+++ app/presenters/content_item/news_image.rb
@@ -0,0 +1,18 @@
+module ContentItem
+  module NewsImage
+    def image
+      content_item.dig("details", "image") || default_news_image || placeholder_image
+    end
+
+  private
+
+    def default_news_image
+      organisation = content_item.dig("links", "primary_publishing_organisation")
+      organisation[0].dig("details", "default_news_image") if organisation.present?
+    end
+
+    def placeholder_image
+      "https://assets.publishing.service.gov.uk/government/assets/placeholder.jpg"
+    end
+  end
+end
