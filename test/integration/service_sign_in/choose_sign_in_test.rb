5a71f64e2c6ffbe0ecfbbac130f72568dcb13cdb
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2019-02-08 10:42:42 +0000

    Fix incorrect assertions on error summary
    This commit fixes the assertions made in `choose_sign_in_test.rb` on the error-summary component provided by govuk_publishing_components. This component was upgraded in version `13.5.3` to use govuk-frontend styles, which in turn broke the assertions made here (see PR https://github.com/alphagov/govuk_publishing_components/pull/692 for details).
    
    Solo: @karlbaker02

--- test/integration/service_sign_in/choose_sign_in_test.rb
+++ test/integration/service_sign_in/choose_sign_in_test.rb
@@ -55,9 +55,9 @@ module ServiceSignIn
 
       click_on 'Continue'
 
-      assert page.has_css?(".gem-c-error-summary")
-      assert page.has_css?(".gem-c-error-summary__title", text: 'You haven’t selected an option')
-      assert page.has_css?(".gem-c-error-summary__link[href='#option-0']", text: 'Please select an option')
+      assert page.has_css?(".govuk-error-summary")
+      assert page.has_css?(".govuk-error-summary__title", text: 'You haven’t selected an option')
+      assert page.has_css?("ul li a:first-of-type[href='#option-0']", text: 'Please select an option')
 
       # Make sure the id is the same as the link href so that they'll link together properly.
       assert page.has_css?(".gem-c-radio input[id='option-0'][value='use-government-gateway']", visible: false)
