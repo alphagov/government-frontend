2a1f0afe626724f688633ff50df467ff8c9c6a49
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2019-02-21 11:28:05 +0000

    Update related links A/B test variant
    This commit updates the A/B test variant for related links from `RelatedLinksABTest1` to `RelatedLinksABTest2`. This is to support the second iteration of the A/B test which is using a new set of related link data.

--- test/controllers/development_controller_test.rb
+++ test/controllers/development_controller_test.rb
@@ -4,8 +4,8 @@ class DevelopmentControllerTest < ActionController::TestCase
   include GovukAbTesting::MinitestHelpers
 
   %w(A B).each do |test_variant|
-    test "RelatedLinksABTest1 works correctly for each variant (variant: #{test_variant})" do
-      with_variant RelatedLinksABTest1: test_variant do
+    test "RelatedLinksABTest2 works correctly for each variant (variant: #{test_variant})" do
+      with_variant RelatedLinksABTest2: test_variant do
         get :index
 
         ab_test = @controller.send(:related_links_test)
