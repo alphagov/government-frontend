6beade89aef03cee50f7d8324714cf9e24c39e7a
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-05-24 16:42:24 +0100

    Remove related links AB test
    This is no longer active.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery except: :service_sign_in_options
 
-  include ABTestable
-
 private
 
   def content_item_path
--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -56,11 +56,6 @@ private
 
   def load_content_item
     content_item = Services.content_store.content_item(content_item_path)
-
-    if related_links_variant.variant?('C') && content_item.dig('links')
-      content_item['links']['ordered_related_items'] = content_item['links'].fetch('suggested_ordered_related_items', [])
-    end
-
     @content_item = PresenterBuilder.new(content_item, content_item_path).presenter
   end
 
