2e2a846f917f73b978359175e1162879bfdaaad0
Rob Bond <robert.bond@digital.cabinet-office.gov.uk>
2019-12-11 15:30:35 +0000

    Tests for Egain class

---
+++ spec/javascripts/webchat-providers/egain.spec.js
@@ -0,0 +1,38 @@
+describe('Egain', function () {
+
+  beforeEach(function () {
+    var egain = new Egain({
+      openUrl: "https://blahchat.gov.uk"
+    });
+  })
+
+  it('should create egain class correctly', function () {
+    expect(egain.openUrl).toBe("https://blahchat.gov.uk")
+    expect(egain.API_STATES).toBe([
+      "BUSY",
+      "UNAVAILABLE",
+      "AVAILABLE",
+      "ERROR"
+    ])
+  }
+
+  it('should return busy if agent is busy', function () {
+    var result = {
+      response: "BUSY"
+    }
+
+    expect(egain.apiResponseSuccess(result).status).toBe("BUSY")
+  }
+
+  it('should return error if invalid state is given', function () {
+    var result = {
+      response: "THIS_IS_INVALID"
+    }
+
+    expect(egain.apiResponseSuccess(result).status).toBe("ERROR")
+  }
+
+  it('should return error for when the API responds with an error', function () {
+    expect(egain.apiResponseError({}).status).toBe("ERROR")
+  }
+}
