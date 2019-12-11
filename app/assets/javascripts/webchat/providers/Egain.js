3ee06894032d1cedb411fb783f30e56cecc1b693
Rob Bond <robert.bond@digital.cabinet-office.gov.uk>
2019-12-11 14:59:18 +0000

    created Egain webchat provider class
    Egain is the default webchat provider used by the contacts application 
    for HMRC

---
+++ app/assets/javascripts/webchat/providers/Egain.js
@@ -0,0 +1,22 @@
+function Egain(options) {
+  this.openUrl = options.openUrl
+  this.API_STATES = [
+    "BUSY",
+    "UNAVAILABLE",
+    "AVAILABLE",
+    "ERROR"
+  ]
+}
+
+Egain.prototype.apiResponseSuccess = function(result) {
+  var validState  = this.API_STATES.indexOf(result.response) != -1
+  return validState ? { status: result.response } : { status: "ERROR" }
+}
+
+Egain.prototype.apiResponseError = function(result) {
+  return { status: "ERROR" };
+}
+
+Egain.prototype.handleOpenChat = function() {
+  global.open(this.openUrl, 'newwin', 'width=200,height=100')
+}
