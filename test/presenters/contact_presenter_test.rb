b200f67407fee63a7c0716820c67e9c8d2ce669a
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-12-18 17:04:09 +0000

    Updates contact present to use web chat model
    This removes any provider specific logic from the
    contact presenter class in favour of using the web chat model
    
    This also updates the view to use the new webchat model to get the
    availability url and open url

--- test/presenters/contact_presenter_test.rb
+++ test/presenters/contact_presenter_test.rb
@@ -88,8 +88,8 @@ class ContactPresenterTest
       schema = schema_item("contact_with_webchat")
       presented = present_example(schema)
       assert_equal true, presented.show_webchat?
-      assert_equal presented.webchat_availability_url, "https://www.tax.service.gov.uk/csp-partials/availability/1030"
-      assert_equal presented.webchat_open_url, "https://www.tax.service.gov.uk/csp-partials/open/1030"
+      assert_equal presented.webchat.availability_url, "https://www.tax.service.gov.uk/csp-partials/availability/1030"
+      assert_equal presented.webchat.open_url, "https://www.tax.service.gov.uk/csp-partials/open/1030"
     end
   end
 end
