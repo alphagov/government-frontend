7310b10d91d120e7d4e2f0ae1f9c36dea0c6673d
maxgds <max.harden@digital.cabinet-office.gov.uk>
2020-03-23 08:48:47 +0000

    Add a variant of the page that warns users of load limits.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require_tree ./modules
 //= require govuk_publishing_components/all_components
 //= require set-ga-client-id-on-form
+//= require coronavirus-extremely-vulnerable
 
 jQuery(function ($) {
   var $form = $('.js-service-sign-in-form')
---
+++ app/assets/javascripts/coronavirus-extremely-vulnerable.js
@@ -0,0 +1,43 @@
+//= require multivariate-test
+
+$(function(){
+  // x minutes in days: x / (60 minutes * 24 hours)
+  var minutesForCohort = 2,
+    minutesInDay = 1440; // 60 * 24
+
+  if(window.location.href.indexOf("/coronavirus-extremely-vulnerable") > -1) {
+    // Get the current cohort or assign one if it has not been already
+    GOVUK.MultivariateTest.prototype.getCohort = function() {
+      var cohort = GOVUK.cookie(this.cookieName());
+      if (!cohort || !this.cohorts[cohort]) {
+        cohort = this.chooseRandomCohort();
+        GOVUK.cookie(this.cookieName(), cohort, {days: minutesForCohort / minutesInDay});
+      }
+      return cohort;
+    };
+
+    GOVUK.MultivariateTest.prototype.setCustomVar = function(cohort) {
+      if (this.customDimensionIndex) {
+        GOVUK.analytics.setDimension(
+          this.customDimensionIndex,
+          this.cookieName() + "__" + cohort
+        );
+      }
+    };
+
+    var bContent = $('#answer-b-content').html();
+    var pcThreshold = 20;
+
+    new GOVUK.MultivariateTest({
+      el: '.answer',
+      name: 'coronavirusExtremelyVulnerable_rateLimit_202003_' + pcThreshold.toString() + 'pc_' + minutesForCohort.toString() + 'm',
+      customDimensionIndex: 14,
+      cohorts: {
+        a: { callback: function() {}, variantId: 0, weight: 100 - pcThreshold },
+        b: { callback: function() {
+          $('.answer').replaceWith(bContent);
+        }, variantId: 1, weight: pcThreshold },
+      }
+    });
+  }
+});
