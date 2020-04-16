2fb0c1f18a4fc9a18b0d4fbedd0fecead6dd731e
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2020-04-16 14:57:15 +0100

    Add different placeholder image for world news
    Adds a new placeholder image for world news stories.
    
    https://govuk.zendesk.com/agent/tickets/3992513

--- app/presenters/content_item/news_image.rb
+++ app/presenters/content_item/news_image.rb
@@ -13,7 +13,11 @@ module ContentItem
 
     def placeholder_image
       # this image has been uploaded to asset-manager
-      { "url" => "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg" }
+      if content_item.dig("document_type") == "world_news_story"
+        { "url" => "https://assets.publishing.service.gov.uk/media/5e985599d3bf7f3fc943bbd8/UK_government_logo.jpg" }
+      else
+        { "url" => "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg" }
+      end
     end
   end
 end
