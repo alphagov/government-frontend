cb0c9b78ae569a48ee84d4d1c84ea411664b5125
Kevin Dew <kevindew@me.com>
2020-05-13 09:45:20 +0100

    Revert "Add GA params to /ask survey link"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,11 @@
 //= require_tree ./modules
 //= require govuk_publishing_components/all_components
-//= require set-ga-client-id-on-url-in-element
+//= require set-ga-client-id-on-form
 
 jQuery(function ($) {
   var $form = $('.js-service-sign-in-form')
-  if ($form.length) {
-    new GOVUK.SetGaClientIdOnUrlInElement({
-      $linkedElement: $form,
-      attribute: 'action'
-    })
-  }
 
-  if ($(location).attr('pathname') === '/ask') {
-    var $link = $('[href="https://www.smartsurvey.co.uk/ss/govuk-coronavirus-ask/"]')
-    new GOVUK.SetGaClientIdOnUrlInElement({
-      $linkedElement: $link,
-      attribute: 'href'
-    })
+  if ($form.length) {
+    new GOVUK.SetGaClientIdOnForm({ $form: $form })
   }
 })
