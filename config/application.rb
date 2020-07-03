837c7e9faf1326cef0ff0fe703df4a8e6341586f
Celia Collins <celiadcollins@gmail.com>
2020-07-03 14:31:08 +0100

    Run `bundle exec rails app:update`
    All the changes that come with Rails 6, mainly comments and minor config changes.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
 require_relative "boot"
 
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
@@ -13,9 +15,13 @@ Bundler.require(*Rails.groups)
 
 module GovernmentFrontend
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
