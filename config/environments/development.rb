958e9582eee36d1aaea8bdfc72e58b043ba0a219
Celia Collins <celiadcollins@gmail.com>
2020-07-03 14:44:50 +0100

    Fix custom alias for local docker development
    Custom alias domains such as http://government-frontend.dev.gov.uk are not
    working on any apps running Rails 6, due to the addition of "Host
    Authorisation" middleware so we need to explicitly add the local domain
    to their `config.hosts`.
    
    Similar PR: alphagov/info-frontend#644
    
    Issue on govuk-docker: alphagov/support#687

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts += [
+    "draft-government-frontend.dev.gov.uk",
+    "government-frontend.dev.gov.uk",
+  ]
 end
