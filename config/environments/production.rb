2e1ca341b316a201159bcfd30bdb0c127f79a001
Evangelos Giataganas <e.giataganas@gmail.com>
2020-08-25 11:26:27 +0100

    Enable the js_compressor option for Sprockets
    Because the default for Rails 6 is to use webpack for JS the Sprockets
    js_compressor option is not set in the generated production.rb
    environment file. This leads to bloated file sizes in production so
    re-enable compression by explicitly setting it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  # Compress JS using a preprocessor.
+  config.assets.js_compressor = :uglifier
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
