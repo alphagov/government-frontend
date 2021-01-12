a58e23d6b7e6a849a64fb963633d9341c46dfd58
Jonathon Shire <jonathon.shire@digital.cabinet-office.gov.uk>
2021-01-12 14:55:27 +0000

    Add helper and presenter for Get Involved migration
    The helper in this case handles a selection of time to string filtering/
    labelling and a single routing function to allow us to link to the
    search page for a given set of consultations (feature match to Whitehall
    original version).
    
    The presenter is empty and is included for consistency with other
    content.

---
+++ app/helpers/get_involved_helper.rb
@@ -0,0 +1,27 @@
+module GetInvolvedHelper
+  def date_microformat(attribute_name)
+      attribute_name.to_date.strftime("%d %B %Y")
+  end
+
+  # Gets the link to the search page for all consultations
+  def get_consultations_link(filters = %w[open_consultations closed_consultations])
+    "search/policy-papers-and-consultations?#{filters.to_query("content_store_document_type")}"
+  end
+
+  private
+
+  def time_until_closure(consultation)
+    days_left = (consultation['details']['closing_date'].to_date - Time.zone.now.to_date).to_i
+    case days_left
+    when :negative?.to_proc
+      "Closed"
+    when :zero?.to_proc
+      "Closing today"
+    when 1
+      "Closes tomorrow"
+    else
+      "#{days_left} days left"
+    end
+  end
+
+end
