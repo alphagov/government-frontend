273ddcd3a1be250cfbc3f495f59c6ca0cc19274b
Thomas Leese <thomas@leese.io>
2021-01-14 12:36:42 +0000

    Ensure asset-manager redirects are cached
    At the moment these redirects aren't cached which means that they won't
    be cached by Fastly and will always hit our origin. This PR should
    prevent this from happening and instead they'll be cached by Fastly for
    1 day.

--- app/controllers/asset_manager_redirect_controller.rb
+++ app/controllers/asset_manager_redirect_controller.rb
@@ -5,6 +5,7 @@ class AssetManagerRedirectController < ApplicationController
       asset_url = Plek.new.external_url_for("draft-assets")
     end
 
+    expires_in 1.day, public: true
     redirect_to host: URI.parse(asset_url).host, status: :moved_permanently
   end
 end
