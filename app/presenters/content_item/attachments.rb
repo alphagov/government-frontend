37b8b4bf079ab48ba3c7d1b525bb5170c08bd769
Kevin Dew <kevindew@me.com>
2021-03-08 11:59:30 +0000

    Remove assumptions of attachment existence
    This resolves a couple of scenarios where assumptions are made that
    documents will have attachments and attachments with particular ids
    exist. This is data that developers assume is the case (and most likely
    is) but is beyond the guarantees that schemas make.
    
    This mismatch can cause tests to fail when valid schemas are generated
    which cannot be rendered.

--- app/presenters/content_item/attachments.rb
+++ app/presenters/content_item/attachments.rb
@@ -1,7 +1,7 @@
 module ContentItem
   module Attachments
     def attachment_details(attachment_id)
-      content_item["details"]["attachments"].find do |attachment|
+      content_item.dig("details", "attachments")&.find do |attachment|
         attachment["id"] == attachment_id
       end
     end
