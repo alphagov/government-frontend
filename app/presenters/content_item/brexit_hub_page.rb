f61f705186099ab477e3f2af2258491ca5c39c49
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-05-27 16:30:14 +0100

    Create helper to check if content item is a brexit hub page

---
+++ app/presenters/content_item/brexit_hub_page.rb
@@ -0,0 +1,29 @@
+module ContentItem
+  module BrexitHubPage
+    BREXIT_BUSINESS_PAGE_CONTENT_ID = "91cd6143-69d5-4f27-99ff-a52fb0d51c78".freeze
+    BREXIT_CITIZEN_PAGE_CONTENT_ID = "6555e0bf-c270-4cf9-a0c5-d20b95fab7f1".freeze
+    BREXIT_HUB_PAGE_CONTENT_IDS = [BREXIT_BUSINESS_PAGE_CONTENT_ID, BREXIT_CITIZEN_PAGE_CONTENT_ID].freeze
+    BREXIT_BUSINESS_PAGE_PATH = "/guidance/brexit-guidance-for-businesses".freeze
+    BREXIT_CITIZEN_PAGE_PATH = "/guidance/brexit-guidance-for-individuals-and-families".freeze
+
+    def brexit_hub_page?
+      BREXIT_HUB_PAGE_CONTENT_IDS.include?(content_item.dig("content_id"))
+    end
+
+    def brexit_business_page?
+      BREXIT_BUSINESS_PAGE_CONTENT_ID == content_item.dig("content_id")
+    end
+
+    def brexit_citizen_page?
+      BREXIT_CITIZEN_PAGE_CONTENT_ID == content_item.dig("content_id")
+    end
+
+    def brexit_citizen_page
+      BREXIT_CITIZEN_PAGE_PATH
+    end
+
+    def brexit_business_page
+      BREXIT_BUSINESS_PAGE_PATH
+    end
+  end
+end
