28a9e9232850bfa99ad7353ac8445edce1ce7b63
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2021-06-24 17:10:29 +0100

    Apply rails app:update changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
-  config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # TODO: Re-enable tests to run against compiled assets with digests
   #       Add a performant assets compilation step before tests run
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   unless Rails.env.production?
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get "random/:schema" => "randomly_generated_content_item#show"
