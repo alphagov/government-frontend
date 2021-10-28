uk-gov-mirror/alphagov.government-frontend

Add new controller logic for the Get Involved Page
The core logic of the controller is a migration of a set of function
calls in whitehall/app/controllers/home_controller.rb for populating the
Get Involved page with relevant data.

Main bulk of controlling calls from Whitehall were taken from here and
applied into this new controller: https://github.com/alphagov/whitehall/blob/bcda05ec43d87b3bec1541e644c1373bafa2a3e8/app/controllers/home_controller.rb#L17-L25

In this case the function calls are replaced with calls to the
publishing-api and respective filtering of those results to give us what
we need in Government Frontend.

app/controllers/get_involved_controller.rb
@@ -1,0 +1,94 @@
+class GetInvolvedController < ApplicationController
+
+  attr_accessor :content_item
+  
+  def show
+    load_content_item
+    load_get_involved_data
+    @do_not_show_breadcrumbs = true
+    render template: "content_items/get_involved"
+  end
+
+  def load_content_item
+    content_item = Services.content_store.content_item("/government/get-involved")
+
+    if Services.feature_toggler.use_recommended_related_links?(content_item["links"], request.headers)
+      content_item["links"]["ordered_related_items"] = content_item["links"].fetch("suggested_ordered_related_items", [])
+    end
+
+    @content_item = PresenterBuilder.new(
+      content_item,
+      content_item_path,
+      view_context,
+    ).presenter
+  end
+
+  def load_get_involved_data
+    @open_consultation_count = retrieve_given_document_type("open_consultation")['total']
+    @closed_consultation_count = retrieve_date_filtered_closed_consultations(12)
+    @next_closing_consultations = [retrieve_next_closing] 
+    @recently_opened_consultations = retrieve_new_consultations
+    @recent_consultation_outcomes = retrieve_consultation_outcomes
+    @take_part_pages = sort_take_part(retrieve_given_document_type("take_part")['results'])
+  end
+
+  def retrieve_given_document_type(document_type)
+    Services.publishing_api.get_content_items(document_type: document_type).to_hash
+  end
+
+  def retrieve_special(params)
+    Services.publishing_api.get_content_items(params).to_hash
+  end
+
+  # Aims to find a count of consultations closed in the last n months.
+  # Could feasibly be done via Search API calls but there is no direct or indirect way to search by closing date
+  # there so we have to go around the houses a bit via a single large Publishing API call.
+  def retrieve_date_filtered_closed_consultations(months)
+    closed_count = 0
+
+    cl_docs = retrieve_special({document_type: 'closed_consultation', per_page: 500, fields: ['details']})['results']
+    cl_docs.sort_by! { |k| -k['details']['closing_date'] }.reverse!
+
+    cl_docs.each do |doc|
+      if DateTime.parse(doc['details']['closing_date']) > DateTime.now.prev_month(months)
+        closed_count += 1
+      else
+        break
+      end
+    end
+
+    return closed_count # hard return for clarity
+  end
+
+  def retrieve_next_closing
+    open_consults = retrieve_given_document_type("open_consultation")['results']
+    open_consults.sort_by! { |k| k['details']['closing_date'] }[0]
+  end
+
+  def retrieve_new_consultations
+    open_consults = retrieve_given_document_type("open_consultation")['results']
+    sorted_desc = open_consults.sort_by! { |k| k['details']['opening_date'] }.reverse!.values_at(0..2)
+    parse_organisation_acronyms(sorted_desc)
+  end
+
+  def retrieve_consultation_outcomes
+    closed_consults = retrieve_given_document_type("consultation_outcome")['results']
+    sorted_desc = closed_consults.sort_by! { |k| k['details']['closing_date'] }.reverse!.values_at(0..2)
+    parse_organisation_acronyms(sorted_desc)
+  end
+
+  def parse_organisation_acronyms(consultations)
+    consultations.each do |consultation|
+      org_acronyms = []
+      organisations = consultation['links']['organisations']
+      organisations.each do |org_id|
+        org_acronyms << Services.publishing_api.get_live_content(org_id).parsed_content['details']['acronym']
+      end
+      consultation['links']['organisation_acronyms'] = org_acronyms
+    end
+  end
+
+  def sort_take_part(take_part_pages)
+    take_part_pages.sort_by{ |page| page["details"]["ordering"] }
+  end
+end

alphagov/government-frontend

app/controllers/get_involved_controller.rb
@@ -1,0 +1,89 @@
+class GetInvolvedController < ContentItemsController
+  attr_accessor :content_item
+
+  attr_accessor :content_item
+  
+  def show
+    load_content_item
+    load_get_involved_data
+    @do_not_show_breadcrumbs = true
+    render template: "content_items/get_involved"
+  end
+
+  def load_content_item
+    content_item = Services.content_store.content_item("/government/get-involved")
+
+    if Services.feature_toggler.use_recommended_related_links?(content_item["links"], request.headers)
+      content_item["links"]["ordered_related_items"] = content_item["links"].fetch("suggested_ordered_related_items", [])
+    end
+
+    @content_item = PresenterBuilder.new(
+      content_item,
+      content_item_path,
+      view_context,
+    ).presenter
+  end
+
+  def load_get_involved_data
+    @open_consultation_count = retrieve_given_document_type("open_consultation")['total']
+    @closed_consultation_count = retrieve_date_filtered_closed_consultations(12)
+    @next_closing_consultations = [retrieve_next_closing] 
+    @recently_opened_consultations = retrieve_new_consultations
+    @recent_consultation_outcomes = retrieve_consultation_outcomes
+    @take_part_pages = sort_take_part(retrieve_given_document_type("take_part")['results'])
+  end
+
+  def retrieve_given_document_type(document_type)
+    Services.publishing_api.get_content_items(document_type: document_type).to_hash
+  end
+
+  # Aims to find a count of consultations closed in the last n months.
+  def retrieve_date_filtered_closed_consultations(months)
+    closed_count = 0
+
+    cl_docs = retrieve_special({document_type: 'closed_consultation', per_page: 500, fields: ['details']})['results']
+    cl_docs.sort_by! { |k| -k['details']['closing_date'] }.reverse!
+
+    cl_docs.each do |doc|
+      if DateTime.parse(doc['details']['closing_date']) > DateTime.now.prev_month(months)
+        closed_count += 1
+      else
+        break
+      end
+    end
+
+    return closed_count # hard return for clarity
+  end
+
+  def retrieve_next_closing
+    open_consults = retrieve_given_document_type("open_consultation")['results']
+    open_consults.sort_by! { |k| k['details']['closing_date'] }[0]
+  end
+
+  def retrieve_new_consultations
+    open_consults = retrieve_given_document_type("open_consultation")['results']
+    sorted_desc = open_consults.sort_by! { |k| k['details']['opening_date'] }.reverse!.values_at(0..2)
+    parse_organisation_acronyms(sorted_desc)
+  end
+
+  def retrieve_consultation_outcomes
+    closed_consults = retrieve_given_document_type("consultation_outcome")['results']
+    sorted_desc = closed_consults.sort_by! { |k| k['details']['closing_date'] }.reverse!.values_at(0..2)
+    parse_organisation_acronyms(sorted_desc)
+  end
+
+  def parse_organisation_acronyms(consultations)
+    consultations.each do |consultation|
+      org_acronyms = []
+      organisations = consultation['links']['organisations']
+      organisations.each do |org_id|
+        org_acronyms << Services.publishing_api.get_live_content(org_id).parsed_content['details']['acronym']
+      end
+      consultation['links']['organisation_acronyms'] = org_acronyms
+    end
+  end
+
+  def sort_take_part(take_part_pages)
+    take_part_pages.sort_by{ |page| page["details"]["ordering"] }
+  end
+end