cef9d63c59349e63f703134891304100223d222e
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2021-11-11 15:02:26 +0000

    Remove tracking for verify users
    What
    
    Remove tracking for verify users
    
    Why
    
    This tracking is no longer required

--- app/assets/javascripts/modules/track-radio-group.js
+++ app/assets/javascripts/modules/track-radio-group.js
@@ -1,12 +1,5 @@
 window.GOVUK = window.GOVUK || {}
-window.GOVUK.Modules = window.GOVUK.Modules || {}
-
-// This callback is triggered by the script attached to the DOM by the
-// checkVerifyUser function. It has to be outside the module because the script
-// can't access the running instance of TrackRadioGroup
-window.GOVUK.TrackRadioGroupVerify = function (data) {
-  window.GOVUK.triggerEvent(document, 'have-verify-data', { detail: data })
-};
+window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function (Modules) {
   function TrackRadioGroup (element) {
@@ -14,25 +7,19 @@ window.GOVUK.TrackRadioGroupVerify = function (data) {
   }
 
   TrackRadioGroup.prototype.init = function () {
-    document.addEventListener('have-verify-data', function (event) {
-      this.reportVerifyUser(this.element, event.detail)
-    }.bind(this))
-
     this.track(this.element)
 
     if (this.crossDomainTrackingEnabled(this.element)) {
       this.addCrossDomainTracking(this.element)
     }
-
-    this.checkVerifyUser(this.element)
   }
 
-  TrackRadioGroup.prototype.track = function (element, withHint) {
+  TrackRadioGroup.prototype.track = function (element) {
     this.element.addEventListener('submit', function (event) {
       var options = { transport: 'beacon' }
       var submittedForm = event.target
       var checkedOption = submittedForm.querySelector('input:checked')
-      var eventValue = this.eventTrackingValue(checkedOption, withHint)
+      var eventValue = this.eventTrackingValue(checkedOption)
 
       GOVUK.analytics.trackEvent('Radio button chosen', eventValue, options)
 
@@ -42,39 +29,13 @@ window.GOVUK.TrackRadioGroupVerify = function (data) {
     }.bind(this))
   }
 
-  // The analytics data should include whether the user has previously signed in
-  // to Verify this simulates the behaviour of JSONP, returning a true or false
-  // value from Verify
-  TrackRadioGroup.prototype.checkVerifyUser = function (element) {
-    var url = 'https://www.signin.service.gov.uk/hint'
-    var timestamp = Math.floor(Date.now() / 1000)
-    var script = document.createElement('script')
-    script.setAttribute('src', url + '?callback=window.GOVUK.TrackRadioGroupVerify&_=' + timestamp)
-    document.body.appendChild(script)
-  }
-
-  TrackRadioGroup.prototype.trackVerifyUser = function (element, data) {
-    this.reportVerifyUser(element, data)
-  }
-
-  TrackRadioGroup.prototype.reportVerifyUser = function (element, data) {
-    if (data != null && data.value === true) {
-      GOVUK.analytics.trackEvent('verify-hint', 'shown', { transport: 'beacon' })
-      this.track(element, data.value)
-    }
-  }
-
-  TrackRadioGroup.prototype.eventTrackingValue = function (element, withHint) {
+  TrackRadioGroup.prototype.eventTrackingValue = function (element) {
     var value = null
 
     if (typeof element === 'undefined' || element === null) {
       value = 'submitted-without-choosing'
     } else {
       value = element.value
-    }
-
-    if (withHint) {
-      value += '-with-hint'
     }
     return value
   }
