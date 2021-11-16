58e67833214a1d46caabc94c2d3c5c7b4de7b057
Chris Yoong <chris.yoong@digital.cabinet-office.gov.uk>
2021-11-16 10:59:52 +0000

    Change "get-involved" Page
    - Update Design of this page
    
    - Use publishing_gem components and Design System and in turn improve consistency and on-going Accessibility 
    
    - Adding inset-text component for use with pull out section that highlights to users a closing consultation
    
    - Removing bespoke "get-involved" CSS
    
    - Change load order of CSS.
    
    The number within "Big number" component was pulling through an unexpected underline from 'govuk-link' on inspection this is coming through from 'component_support' - in this context it's due to the cascade, by moving the load order this resolve this issue as "big numbers" is being loaded after resolving the issue.
    
    - Adjust layout for "take part" section that uses image_card component grid. As this is a list this uses an adjusted technique while a layout feature request on the Design System is outstanding,  This allows the Design system grid to be used but AT to interpret the section as a list [1]
    
    [1] https://github.com/alphagov/govuk-frontend/issues/2328

--- app/controllers/get_involved_controller.rb
+++ app/controllers/get_involved_controller.rb
@@ -5,6 +5,8 @@ class GetInvolvedController < ContentItemsController
     load_content_item
     load_get_involved_data
     @do_not_show_breadcrumbs = true
+    @recently_opened = filtered_links(@recently_opened_consultations, t("get_involved.closes"))
+    @recent_outcomes = filtered_links(@recent_consultation_outcomes, t("get_involved.closed"))
     render template: "content_items/get_involved"
   end
 
@@ -87,5 +89,53 @@ class GetInvolvedController < ContentItemsController
     }
 
     Services.search_api.search(query)["results"]
+  end
+
+  def time_until_closure(consultation)
+    days_left = (consultation["end_date"].to_date - Time.zone.now.to_date).to_i
+    case days_left
+    when :negative?.to_proc
+      t("get_involved.closed")
+    when :zero?.to_proc
+      t("get_involved.closing_today")
+    when 1
+      t("get_involved.closing_tomorrow")
+    else
+      t("get_involved.days_left", number_of_days: days_left)
+    end
+  end
+
+  def date_microformat(attribute_name)
+    attribute_name.to_date.strftime("%d %B %Y")
+  end
+
+  def filtered_links(array, close_status)
+    array.map do |item|
+      {
+        link: {
+          text: item["title"],
+          path: item["link"],
+          description: "#{close_status} #{date_microformat(item['end_date'])}",
+        },
+        metadata: {
+          public_updated_at: Time.zone.parse(org_acronym(item)),
+          document_type: org_acronym(item),
+        },
+      }
+    end
+  end
+
+private
+
+  def org_acronym(item)
+    item["organisations"].map { |org|
+      org["public_timestamp"]
+    }.join(", ")
+  end
+
+  def org_time(item)
+    item["organisations"].map { |org|
+      org["acronym"]
+    }.join(", ")
   end
 end
