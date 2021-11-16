4c883d141dd1018e9f111a5c57b59c14bc5cbf57
Dana Cotoran <dana.cotoran@gmail.com>
2021-11-16 09:43:58 +0000

    Add the notification button to publication template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/print-link
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/single-page-notification-button
 //= require govuk_publishing_components/components/step-by-step-nav
 
 //= require_tree ./modules
