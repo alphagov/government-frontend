db6608ee7b7b2a4496a3193bb958983f57cc699a
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2022-01-06 11:58:38 +0000

    Revert "Force English for metadata"
    This reverts commit 20171564e8e23470ad203021ecdf4ae87e13316a.
    
    Relevant keys have now been translated to this change can be reverted.
    
    Trello: https://trello.com/c/4rRSo056/2794-re-enable-translated-dates-3

--- app/presenters/content_item_presenter.rb
+++ app/presenters/content_item_presenter.rb
@@ -99,7 +99,7 @@ private
   end
 
   def display_date(timestamp, format = "%-d %B %Y")
-    I18n.l(Time.zone.parse(timestamp), format: format, locale: "en") if timestamp
+    I18n.l(Time.zone.parse(timestamp), format: format) if timestamp
   end
 
   def sorted_locales(translations)
