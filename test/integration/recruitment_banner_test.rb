c7d5cd09d042a98bfa5d30fa3d531742d1d94449
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-03-14 17:22:43 +0000

    Render the intervention banner on any page tagged to "browse/business"
    At this time, we are only using a single survey URL, but this might change
    in the coming weeks so that different topic pages require links to different
    surveys.

--- test/integration/recruitment_banner_test.rb
+++ test/integration/recruitment_banner_test.rb
@@ -19,8 +19,34 @@ class RecruitmentBannerTest < ActionDispatch::IntegrationTest
     assert page.has_link?("Take part in user research (opens in a new tab)", href: "https://GDSUserResearch.optimalworkshop.com/treejack/724268fr-1")
   end
 
-  test "Recruitment Banner is not displayed for pages not tagged to Money and Tax" do
+  test "Recruitment Banner is displayed for any page tagged to Business and Self-employed" do
+    @business_browse_page = {
+      "content_id" => "123",
+      "title" => "Self Assessment",
+      "base_path" => "/browse/business",
+    }
+
     guide = GovukSchemas::Example.find("guide", example_name: "guide")
+    guide["links"]["mainstream_browse_pages"] = []
+    guide["links"]["mainstream_browse_pages"] << @business_browse_page
+
+    stub_content_store_has_item(guide["base_path"], guide.to_json)
+    visit guide["base_path"]
+
+    assert page.has_css?(".gem-c-intervention")
+    assert page.has_link?("Take part in user research (opens in a new tab)", href: "https://GDSUserResearch.optimalworkshop.com/treejack/724268fr-1")
+  end
+
+  test "Recruitment Banner is not displayed unless page is tagged to a topic of interest" do
+    @not_of_interest = {
+      "content_id" => "123",
+      "title" => "I am not interesting",
+      "base_path" => "/browse/boring",
+    }
+
+    guide = GovukSchemas::Example.find("guide", example_name: "guide")
+    guide["links"]["mainstream_browse_pages"] = []
+    guide["links"]["mainstream_browse_pages"] << @not_of_interest
     stub_content_store_has_item(guide["base_path"], guide.to_json)
     visit_with_cachebust guide["base_path"]
 
