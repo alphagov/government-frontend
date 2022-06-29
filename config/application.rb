a76cf34ab2ccf5b2cbbe2b8b8e67534d629ae5f5
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2022-06-29 15:27:28 +0100

    Switch to Rails 7 config defaults

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module GovernmentFrontend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -29,6 +29,5 @@ module GovernmentFrontend
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
