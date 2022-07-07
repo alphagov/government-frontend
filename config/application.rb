278eebf088a8d92c7cca1e098194d7f06514761d
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2022-07-07 16:35:26 +0100

    Disable unsafe redirect protection
    The redirects are to within *.gov.uk

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module GovernmentFrontend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Disable redirect protection. This is turned on by default in Rails 7.0.
+    config.action_controller.raise_on_open_redirects = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
