d4a18d89250a1550a81738baae0b6090ec2c36f2
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2022-07-18 16:39:19 +0100

    Explicitly allow redirects to other hosts within gov.uk domain
    In rails 7 redirects to other hosts are no longer permitted by default and ActionController::Redirecting::UnsafeRedirectError was raised.
    
    However in all those cases we are redirect to other GOV.UK domains such as
    assets.gov.uk or service.gov.uk.

--- app/controllers/asset_manager_redirect_controller.rb
+++ app/controllers/asset_manager_redirect_controller.rb
@@ -2,6 +2,9 @@ class AssetManagerRedirectController < ApplicationController
   def show
     asset_url = Plek.new.external_url_for("assets")
     expires_in 1.day, public: true
-    redirect_to host: URI.parse(asset_url).host, status: :moved_permanently
+    redirect_to(
+      { host: URI.parse(asset_url).host, status: :moved_permanently },
+      allow_other_host: true,
+    )
   end
 end
