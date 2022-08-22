2cd8dc49b61244ad1f511f4aff3911818f5b137f
Tetrino <jonathon.shire@digital.cabinet-office.gov.uk>
2022-08-22 08:13:04 +0100

    Add the service standard presenter and template.

---
+++ app/presenters/service_manual_service_standard_presenter.rb
@@ -0,0 +1,56 @@
+class ServiceManualServiceStandardPresenter < ContentItemPresenter
+  attr_reader :poster_url
+
+  def initialize(*)
+    super
+    @poster_url = details["poster_url"]
+  end
+
+  def points
+    Point.load(points_attributes).sort
+  end
+
+  def breadcrumbs
+    [
+      { title: "Service manual", url: "/service-manual" },
+    ]
+  end
+
+  def email_alert_signup_link
+    "/email-signup?link=#{content_item['base_path']}"
+  end
+
+private
+
+  def points_attributes
+    @points_attributes ||= links["children"] || []
+  end
+
+  class Point
+    include Comparable
+
+    attr_reader :title, :description, :base_path
+
+    def self.load(points_attributes)
+      points_attributes.map { |point_attributes| new(point_attributes) }
+    end
+
+    def initialize(attributes)
+      @title = attributes["title"]
+      @description = attributes["description"]
+      @base_path = attributes["base_path"]
+    end
+
+    def <=>(other)
+      number <=> other.number
+    end
+
+    def title_without_number
+      @title_without_number ||= title.sub(/\A(\d*)\.(\s*)/, "")
+    end
+
+    def number
+      @number ||= Integer(title.scan(/\A(\d*)/)[0][0])
+    end
+  end
+end
