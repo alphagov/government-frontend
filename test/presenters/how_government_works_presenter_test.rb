83eeef312071ee7dd58e1fb4d33beb8d693e07b5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-03-09 14:43:22 +0000

    Add 'How Government Works' presenter
    This will be used to get the relevant pieces of information from the
    content item for this page.

---
+++ test/presenters/how_government_works_presenter_test.rb
@@ -0,0 +1,31 @@
+require "presenter_test_helper"
+
+class HowGovernmentWorksPresenterTest < PresenterTestCase
+  def schema_name
+    "how_government_works"
+  end
+
+  test "presents the basic details of a content item" do
+    assert_equal schema_item("reshuffle-mode-off")["title"], presented_item("reshuffle-mode-off").title
+    assert_equal schema_item("reshuffle-mode-off")["description"], presented_item("reshuffle-mode-off").description
+  end
+
+  test "presents the current prime minister" do
+    assert_equal(
+      schema_item("reshuffle-mode-off").dig("links", "current_prime_minister", 0, "title"),
+      presented_item("reshuffle-mode-off").prime_minister,
+    )
+  end
+
+  test "presents the presence of reshuffle mode" do
+    assert_equal schema_item("reshuffle-mode-on")["details"]["reshuffle_in_progress"], presented_item("reshuffle-mode-on").reshuffle_in_progress?
+  end
+
+  test "presents the ministerial role count" do
+    assert_equal schema_item("reshuffle-mode-off")["details"]["ministerial_role_counts"], presented_item("reshuffle-mode-off").ministerial_role_counts
+  end
+
+  test "presents the department count" do
+    assert_equal schema_item("reshuffle-mode-off")["details"]["department_counts"], presented_item("reshuffle-mode-off").department_counts
+  end
+end
