9d40e567f77332866d17f16d29a9b7822144381e
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2023-03-10 13:11:49 +0000

    Add rendering of field of operation pages
    This is part of some longstanding work to remove rendering from whitehall
    
    It required me to add some i18n, unfortunately running the task to add missing locales also fixed some speech marks that were off the wrong type for the generation. Sorry for the extra noise!

---
+++ app/presenters/field_of_operation_presenter.rb
@@ -0,0 +1,53 @@
+class FieldOfOperationPresenter < ContentItemPresenter
+  include ContentItem::TitleAndContext
+
+  FatalityNotice = Struct.new(:roll_call_introduction, :casualties, :title, :base_path)
+
+  def title_and_context
+    super.tap do |t|
+      t[:context] = I18n.t("field_of_operation.context")
+      t[:title] = "#{I18n.t('field_of_operation.title')} #{@content_item['title']}"
+      t.delete(:average_title_length)
+    end
+  end
+
+  def organisation
+    org = @content_item.dig("links", "primary_publishing_organisation", 0)
+    logo = org.dig("details", "logo")
+
+    {
+      name: logo["formatted_title"].html_safe,
+      url: org["base_path"],
+      brand: org.dig("details", "brand"),
+      crest: logo["crest"],
+    }
+  end
+
+  def description
+    description = @content_item["description"]
+
+    description.html_safe if description.present?
+  end
+
+  def fatality_notices
+    notices = @content_item.dig("links", "fatality_notices")
+    return [] unless notices
+
+    notices.map do |notice|
+      FatalityNotice.new(
+        notice.dig("details", "roll_call_introduction"),
+        notice.dig("details", "casualties"),
+        notice["title"],
+        notice["base_path"],
+      )
+    end
+  end
+
+  def contents
+    contents = []
+    contents << { href: "#field-of-operation", text: "Field of operation" } if description.present?
+    contents << { href: "#fatalities", text: "Fatalities" } if fatality_notices.present?
+
+    contents
+  end
+end
