68cb888b371fbca2487936b8e65b8b52d307a59e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-03-23 14:44:40 +0000

    Add tests to prevent regression
    Accessibility issue in the past where there were two elements with an id
    of 'content'. Added small test to ensure that this is not reintroduced
    again.

--- test/integration/hmrc_manual_section_test.rb
+++ test/integration/hmrc_manual_section_test.rb
@@ -12,6 +12,13 @@ class HmrcManualSectionTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "partial has 1 content id" do
+    setup_and_visit_manual_section
+    content_ids = page.all('[id="content"]')
+
+    assert_equal 1, content_ids.count
+  end
+
   test "renders metadata" do
     setup_and_visit_manual_section
 
