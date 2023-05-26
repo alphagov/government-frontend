8604e193f8d5aa0f001decca1078c8dd01c57dfa
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-05-26 14:52:02 +0100

    Render "Our people" section
    We are using the standard component image card, which makes the size of
    the description text slightly bigger than the current rendition in
    whitehall.

--- app/presenters/worldwide_organisation_presenter.rb
+++ app/presenters/worldwide_organisation_presenter.rb
@@ -8,4 +8,40 @@ class WorldwideOrganisationPresenter < ContentItemPresenter
   def social_media_accounts
     content_item.dig("details", "social_media_links") || []
   end
+
+  def show_our_people_section?
+    person_in_primary_role || people_in_non_primary_roles.any?
+  end
+
+  def person_in_primary_role
+    return unless content_item["links"]["primary_role_person"]
+
+    person = content_item.dig("links", "primary_role_person").first
+    current_roles = person.dig("links", "role_appointments").select { |role_app| role_app.dig("details", "current") }
+
+    {
+      name: person["title"],
+      href: person["web_url"],
+      image_url: person["details"]["image"]["url"],
+      image_alt: person["details"]["image"]["alt_text"],
+      description: current_roles.map { |role_app| role_app.dig("links", "role").first["title"] }.join,
+    }
+  end
+
+  def people_in_non_primary_roles
+    secondary_role_person = content_item["links"]["secondary_role_person"] || []
+    office_staff = content_item["links"]["office_staff"] || []
+    people = secondary_role_person + office_staff
+    return [] unless people.any?
+
+    people.map do |person|
+      current_roles = person.dig("links", "role_appointments").select { |role_app| role_app.dig("details", "current") }
+
+      {
+        name: person["title"],
+        href: person["web_url"],
+        description: current_roles.map { |role_app| role_app.dig("links", "role").first["title"] }.join,
+      }
+    end
+  end
 end
--- test/integration/worldwide_organisation_test.rb
+++ test/integration/worldwide_organisation_test.rb
@@ -18,4 +18,28 @@ class WorldwideOrganisationTest < ActionDispatch::IntegrationTest
     assert page.has_selector?("#about-us")
     assert page.has_text?("Find out more on our UK and India")
   end
+
+  test "renders the person in the primary role" do
+    setup_and_visit_content_item("worldwide_organisation")
+    assert page.has_link?("Karen Pierce DCMG", href: "https://www.integration.publishing.service.gov.uk/government/people/karen-pierce")
+    assert page.has_css?("img[src=\"https://assets.publishing.service.gov.uk/government/uploads/system/uploads/person/image/583/s216_UKMissionGeneva__HMA_Karen_Pierce_-_uploaded.jpg\"]")
+  end
+
+  test "renders people in secondary and office roles" do
+    setup_and_visit_content_item("worldwide_organisation")
+    assert page.has_link?("Justin Sosne", href: "https://www.integration.publishing.service.gov.uk/government/people/justin-sosne")
+    assert page.has_link?("Rachel Galloway", href: "https://www.integration.publishing.service.gov.uk/government/people/rachel-galloway")
+  end
+
+  test "doesn't render the people section if there are no appointed people" do
+    setup_and_visit_content_item(
+      "worldwide_organisation",
+      { "links" => {
+        "primary_role_person" => nil,
+        "secondary_role_person" => nil,
+        "office_staff" => nil,
+      } },
+    )
+    assert_not page.has_text?("Our people")
+  end
 end
