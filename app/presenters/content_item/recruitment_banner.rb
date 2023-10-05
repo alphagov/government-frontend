b38b7d5e9507246d7d2ce9cf73cf880a4a94822e
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-10-05 12:15:57 +0100

    Add user research banner to benefits pages

--- app/presenters/content_item/recruitment_banner.rb
+++ app/presenters/content_item/recruitment_banner.rb
@@ -12,8 +12,32 @@ module ContentItem
       "/government/organisations/hm-revenue-customs/contact/self-assessment" => SURVEY_URL,
     }.freeze
 
+    BENEFITS_SURVEY_URL = "https://signup.take-part-in-research.service.gov.uk/home?utm_campaign=Content_History&utm_source=Hold_gov_to_account&utm_medium=gov.uk&t=GDS&id=16".freeze
+    BENEFITS_SURVEY_URL_MAPPINGS = {
+      "/disability-living-allowance-children" => BENEFITS_SURVEY_URL,
+      "/help-with-childcare-costs" => BENEFITS_SURVEY_URL,
+      "/financial-help-disabled" => BENEFITS_SURVEY_URL,
+      "/pip" => BENEFITS_SURVEY_URL,
+      "/blind-persons-allowance" => BENEFITS_SURVEY_URL,
+      "/dla-disability-living-allowance-benefit" => BENEFITS_SURVEY_URL,
+      "/carers-allowance" => BENEFITS_SURVEY_URL,
+      "/carers-credit" => BENEFITS_SURVEY_URL,
+      "/maternity-pay-leave" => BENEFITS_SURVEY_URL,
+      "/paternity-pay-leave" => BENEFITS_SURVEY_URL,
+      "/child-benefit" => BENEFITS_SURVEY_URL,
+      "/jobseekers-allowance" => BENEFITS_SURVEY_URL,
+      "/universal-credit" => BENEFITS_SURVEY_URL,
+      "/employment-support-allowance" => BENEFITS_SURVEY_URL,
+      "/benefits-calculators" => BENEFITS_SURVEY_URL,
+    }.freeze
+
     def recruitment_survey_url
       user_research_test_url
+    end
+
+    def benefits_recruitment_survey_url
+      key = content_item["base_path"]
+      BENEFITS_SURVEY_URL_MAPPINGS[key]
     end
 
     def user_research_test_url
