babdd8c576314c690b05840ad88775d8aa0acb92
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-10-11 14:29:20 +0100

    Configure builds
    - Create `dartsass.builds` initializer and add all Sass files to be compiled. See https://github.com/rails/dartsass-rails#configuring-builds
    - Create .keep
    - Update manifest file to use /builds directory
    - Ignore /builds

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,24 @@
+APP_STYLESHEETS = {
+  "application.scss" => "application.css",
+  "components/_back-to-top.scss" => "components/_back-to-top.css",
+  "components/_banner.scss" => "components/_banner.css",
+  "components/_contents-list-with-body.scss" => "components/_contents-list-with-body.css",
+  "components/_download-link.scss" => "components/_download-link.css",
+  "components/_error-message.scss" => "components/_error-message.css",
+  "components/_figure.scss" => "components/_figure.css",
+  "components/_important-metadata.scss" => "components/_important-metadata.css",
+  "components/_published-dates.scss" => "components/_published-dates.css",
+  "components/_publisher-metadata.scss" => "components/_publisher-metadata.css",
+  "views/_guide.scss" => "views/_guide.css",
+  "views/_html-publication.scss" => "views/_html-publication.css",
+  "views/_manual.scss" => "views/_manual.css",
+  "views/_published-dates-button-group.scss" => "views/_published-dates-button-group.css",
+  "views/_service_manual_guide.scss" => "views/_service_manual_guide.css",
+  "views/_specialist-document.scss" => "views/_specialist-document.css",
+  "views/_travel-advice.scss" => "views/_travel-advice.css",
+}.freeze
+
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+Rails.application.config.dartsass.builds = all_stylesheets
+
+Rails.application.config.dartsass.build_options << " --quiet-deps"
