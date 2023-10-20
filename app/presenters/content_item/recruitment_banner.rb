ce73931ddbe65ba944d14ddbc95bdd2c52ca5138
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-10-20 15:42:09 +0100

    Add UR banner details in the yml file

--- app/presenters/content_item/recruitment_banner.rb
+++ app/presenters/content_item/recruitment_banner.rb
@@ -1,10 +1,12 @@
 module ContentItem
   module RecruitmentBanner
-    def recruitment_survey_url
-      pages = YAML.load_file(Rails.root.join("config/recruitment_banner_pages.yml"))
+    def recruitment_survey_details
+      survey_pages = YAML.load_file(Rails.root.join("config/recruitment_banner_pages.yml"))
       key = content_item["base_path"]
 
-      pages[key] if pages
+      # TODO: check if all the values are in place
+      # TODO: if the text is not specified in the yml, we should use the standard one
+      survey_pages.find{ |banner| banner["pages"].include?(key) } if survey_pages
     end
   end
 end
