443ba2173e02e0b570fd07a81a819e0ead42dc9c
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-10-26 11:20:38 +0100

    Remove the research banner

--- app/presenters/content_item/recruitment_banner.rb
+++ app/presenters/content_item/recruitment_banner.rb
@@ -1,17 +1,5 @@
 module ContentItem
   module RecruitmentBanner
-    SURVEY_URL = "https://surveys.publishing.service.gov.uk/s/SNFVW1/".freeze
-    SURVEY_URL_MAPPINGS = {
-      "/log-in-register-hmrc-online-services" => SURVEY_URL,
-      "/log-in-file-self-assessment-tax-return" => SURVEY_URL,
-      "/self-assessment-tax-returns" => SURVEY_URL,
-      "/pay-self-assessment-tax-bill" => SURVEY_URL,
-      "/contact-hmrc" => SURVEY_URL,
-      "/log-in-register-hmrc-online-services/register" => SURVEY_URL,
-      "/dbs-update-service" => SURVEY_URL,
-      "/government/organisations/hm-revenue-customs/contact/self-assessment" => SURVEY_URL,
-    }.freeze
-
     BENEFITS_SURVEY_URL = "https://signup.take-part-in-research.service.gov.uk/home?utm_campaign=Content_History&utm_source=Hold_gov_to_account&utm_medium=gov.uk&t=GDS&id=16".freeze
     BENEFITS_SURVEY_URL_MAPPINGS = {
       "/disability-living-allowance-children" => BENEFITS_SURVEY_URL,
@@ -31,18 +19,9 @@ module ContentItem
       "/benefits-calculators" => BENEFITS_SURVEY_URL,
     }.freeze
 
-    def recruitment_survey_url
-      user_research_test_url
-    end
-
     def benefits_recruitment_survey_url
       key = content_item["base_path"]
       BENEFITS_SURVEY_URL_MAPPINGS[key]
-    end
-
-    def user_research_test_url
-      key = content_item["base_path"]
-      SURVEY_URL_MAPPINGS[key]
     end
   end
 end
--- test/integration/recruitment_banner_test.rb
+++ test/integration/recruitment_banner_test.rb
@@ -1,31 +1,6 @@
 require "test_helper"
 
 class RecruitmentBannerTest < ActionDispatch::IntegrationTest
-  test "User research banner is displayed on pages of interest" do
-    guide = GovukSchemas::Example.find("guide", example_name: "guide")
-
-    pages_of_interest =
-      [
-        "/log-in-register-hmrc-online-services",
-        "/log-in-file-self-assessment-tax-return",
-        "/self-assessment-tax-returns",
-        "/pay-self-assessment-tax-bill",
-        "/contact-hmrc",
-        "/log-in-register-hmrc-online-services/register",
-        "/dbs-update-service",
-        "/government/organisations/hm-revenue-customs/contact/self-assessment",
-      ]
-
-    pages_of_interest.each do |path|
-      guide["base_path"] = path
-      stub_content_store_has_item(guide["base_path"], guide.to_json)
-      visit guide["base_path"]
-
-      assert page.has_css?(".gem-c-intervention")
-      assert page.has_link?("Sign up to take part in user research", href: "https://surveys.publishing.service.gov.uk/s/SNFVW1/")
-    end
-  end
-
   test "Benefits user research banner is displayed on pages of interest" do
     guide = GovukSchemas::Example.find("guide", example_name: "guide")
 
