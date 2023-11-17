65006989f9826fc5777cd3f3e15eebaca3efa4f3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2023-11-17 14:02:17 +0000

    Remove ga4_tracking: true component options
    - all components that previously accepted this option now have GA4 tracking enabled by default, so these options are no longer needed
    - no tracking should change as a result of this change

--- app/helpers/service_manual_phase_label_helper.rb
+++ app/helpers/service_manual_phase_label_helper.rb
@@ -3,7 +3,6 @@ module ServiceManualPhaseLabelHelper
     if presented_object.respond_to?(:phase) && %w[alpha beta].include?(presented_object.phase)
       render "govuk_publishing_components/components/phase_banner",
              phase: presented_object.phase,
-             ga4_tracking: true,
              message:
     end
   end
