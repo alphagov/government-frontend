d99cbbbb30f2ff6594104cecbaef2216bbd1c003
George Schena <george.schena1@digital.cabinet-office.gov.uk>
2023-12-28 11:09:31 +0000

    Dry up Recruitment Banner logic

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -4,6 +4,7 @@ class ContentItemsController < ApplicationController
   include GovukPersonalisation::ControllerConcern
   include Slimmer::Headers
   include Slimmer::Template
+  include RecruitmentBannerHelper
 
   rescue_from GdsApi::HTTPForbidden, with: :error_403
   rescue_from GdsApi::HTTPNotFound, with: :error_notfound
