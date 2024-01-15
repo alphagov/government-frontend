1ff8c202a1cbdc1c6c63fbe8187e933a8cbfc7c9
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2024-01-15 13:16:10 +0000

    Run Rails app:update
    Updates the rails app correctly to bring it in line with 7.1 using
    the rails updater.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,7 +14,3 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym "RESTful"
 # end
-
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.singular "how_government_works", "how_government_works"
-end
