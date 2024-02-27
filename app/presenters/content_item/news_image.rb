822511ac8bf2419f0739c84ad8ff81ec8f90fef6
Joseph Kempster <jkempster34@gmail.com>
2024-02-27 15:55:44 +0000

    DRY up references to world news story
    The placeholder image and default news image both depend on whether the content
    item is a world news story.

--- app/presenters/content_item/news_image.rb
+++ app/presenters/content_item/news_image.rb
@@ -7,16 +7,14 @@ module ContentItem
   private
 
     def default_news_image
-      if content_item["document_type"].eql? "world_news_story"
-        return first_worldwide_organisation_default_news_image
-      end
+      return first_worldwide_organisation_default_news_image if world_news_story?
 
       first_primary_publishing_organisation_default_news_image
     end
 
     def placeholder_image
       # this image has been uploaded to asset-manager
-      if content_item["document_type"] == "world_news_story"
+      if world_news_story?
         { "url" => "https://assets.publishing.service.gov.uk/media/5e985599d3bf7f3fc943bbd8/UK_government_logo.jpg" }
       else
         { "url" => "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg" }
@@ -31,6 +29,10 @@ module ContentItem
     def first_primary_publishing_organisation_default_news_image
       organisation = content_item.dig("links", "primary_publishing_organisation")
       organisation[0].dig("details", "default_news_image") if organisation.present?
+    end
+
+    def world_news_story?
+      content_item["document_type"] == "world_news_story"
     end
   end
 end
