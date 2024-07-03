f2ed9ab1bb111776f1e5ea210cf46d914096eeef
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-07-03 12:05:25 +0100

    Convert locales_validation tests
    - add code to suppress the "good job!" message

--- test/unit/locales_validation_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class LocalesValidationTest < ActiveSupport::TestCase
-  test "should validate all locale files" do
-    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*.yml")
-    assert checker.validate_locales
-  end
-end
