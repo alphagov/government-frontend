alphagov/government-frontend

commit 8128fb27b10e1895726042041916119cdc145694

Fix missing headings and replace subpage breadcrumb with back link
* Replaced the landing-page breadcrumb with H2
* Replaced the sub-page breadcrumb with the back link component
* Added a "Contents" H2 above the document list
* Rewrote tests accordingly

app/views/content_items/manuals/_manual_section_layout.html.erb
@@ -1,6 +1,10 @@
 <% show_contents ||= false %>
 
 <% content_for :main do %>
+  <%= render "govuk_publishing_components/components/back_link", {
+    text: t("manuals.breadcrumb_contents"),
+    href: @content_item.base_path
+  } %>
   <div id="manuals-frontend" class="manuals-frontend-body">
     <% if show_contents %>
       <%= render "govuk_publishing_components/components/contents_list", {

test/integration/hmrc_manual_section_test.rb
@@ -42,13 +42,10 @@ class HmrcManualSectionTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "renders manual specific breadcrumbs" do
+  test "renders back link" do
     setup_and_visit_manual_section
 
-    manual_specific_breadcrumbs = page.all(".gem-c-breadcrumbs")[1]
-    within manual_specific_breadcrumbs do
-      assert page.has_text?(I18n.t("manuals.breadcrumb_contents"))
-    end
+    assert page.has_link?(I18n.t("manuals.breadcrumb_contents"), href: "/hmrc-internal-manuals/vat-government-and-public-bodies")
   end
 
   test "renders section group" do

commit 70f902a2d905b25e991ddf900604eea6a4ed6518

Display breadcrumbs if they are present
A bug was introduced in 8128fb27b10e1895726042041916119cdc145694
so that breadcrumbs were no longer being displayed on the page.
If breadcrumbs are present in the content item, we should display them.
If they're absent, display a back link to the parent manual.

app/views/content_items/manuals/_manual_section_layout.html.erb
@@ -1,10 +1,17 @@
 <% show_contents ||= false %>
 
 <% content_for :main do %>
-  <%= render "govuk_publishing_components/components/back_link", {
-    text: t("manuals.breadcrumb_contents"),
-    href: @content_item.base_path
-  } %>
+  <% if @content_item.breadcrumbs.any? %>
+    <%= render "govuk_publishing_components/components/breadcrumbs", {
+           border: "bottom",
+           breadcrumbs: @content_item.breadcrumbs,
+           collapse_on_mobile: false } %>
+  <% else %>
+    <%= render "govuk_publishing_components/components/back_link", {
+      text: t("manuals.breadcrumb_contents"),
+      href: @content_item.base_path
+    } %>
+  <% end %>
   <div id="manuals-frontend" class="manuals-frontend-body">
     <% if show_contents %>
       <%= render "govuk_publishing_components/components/contents_list", {

test/integration/hmrc_manual_section_test.rb
@@ -42,10 +42,16 @@ class HmrcManualSectionTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "renders back link" do
+  test "renders back link if breadcrumbs are not set" do
     setup_and_visit_manual_section
 
     assert page.has_link?(I18n.t("manuals.breadcrumb_contents"), href: "/hmrc-internal-manuals/vat-government-and-public-bodies")
+  end
+
+  test "renders breadcrumbs if they are set in the content item" do
+    setup_and_visit_manual_section_with_breadcrumbs
+
+    assert page.has_link?("DMBM510000", href: "/hmrc-internal-manuals/debt-management-and-banking/dmbm510000")
   end
 
   test "renders section group" do
@@ -90,6 +96,22 @@ class HmrcManualSectionTest < ActionDispatch::IntegrationTest
     stub_content_store_has_item(manual_base_path, @manual.to_json)
 
     stub_content_store_has_item(@content_item["base_path"], @content_item.to_json)
+    visit_with_cachebust((@content_item["base_path"]).to_s)
+  end
+
+  def setup_and_visit_manual_section_with_breadcrumbs(content_item = get_content_example("vatgpb2000"))
+    @manual = get_content_example_by_schema_and_name("hmrc_manual", "vat-government-public-bodies")
+    @content_item = content_item
+    first_crumb = { "base_path" => "/hmrc-internal-manuals/debt-management-and-banking/dmbm510000", "section_id" => "DMBM510000" }
+    last_crumb = { "base_path" => "/hmrc-internal-manuals/vat-government-public-bodiesg/dmbm510100", "section_id" => "DMBM510100" }
+
+    @content_item["details"]["breadcrumbs"].push(first_crumb, last_crumb)
+    manual_base_path = @content_item["details"]["manual"]["base_path"]
+
+    stub_content_store_has_item(manual_base_path, @manual.to_json)
+
+    stub_content_store_has_item(@content_item["base_path"], @content_item.to_json)
+    stub_content_store_has_item(@content_item["details"]["breadcrumbs"].last["base_path"], @content_item.to_json)
     visit_with_cachebust((@content_item["base_path"]).to_s)
   end
 end