e154f88eb826e58ae443690e2b076b7f29d99480
AshGDS <8880610+AshGDS@users.noreply.github.com>
2024-09-05 11:35:56 +0100

    Move es6-components.js back into application.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
 //= link application.js
-//= link es6-components.js
 //= link webchat.js
 //= link_tree ../builds
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,12 @@
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/image-card
 //= require govuk_publishing_components/components/intervention
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/print-link
+//= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/single-page-notification-button
 //= require govuk_publishing_components/components/step-by-step-nav
 
