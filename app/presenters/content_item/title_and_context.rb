466675f780f652a640ac3c5b00c9aecf61b27cdc
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-02-06 09:27:12 +0000

    Replace title component with heading component

--- app/presenters/content_item/title_and_context.rb
+++ app/presenters/content_item/title_and_context.rb
@@ -2,10 +2,12 @@ module ContentItem
   module TitleAndContext
     def title_and_context
       {
-        title:,
+        text: title,
         context: I18n.t("content_item.schema_name.#{document_type}", count: 1),
         context_locale: view_context.t_locale_fallback("content_item.schema_name.#{document_type}", count: 1),
-        average_title_length: "long",
+        heading_level: 1,
+        margin_bottom: 8,
+        font_size: "l",
       }
     end
   end
