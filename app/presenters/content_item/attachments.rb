71345cd813edd23078bade6cebe33663aaaf2778
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2025-02-24 16:17:30 +0000

    Change attachment_data_id to asset_manager_id

--- app/presenters/content_item/attachments.rb
+++ app/presenters/content_item/attachments.rb
@@ -21,14 +21,14 @@ module ContentItem
 
     def preview_url(doc)
       case doc.symbolize_keys
-      in { preview_url: String, attachment_data_id:, filename: }
-        # We have attachment_data_id and filename, so we can redirect to a new style CSV preview
+      in { preview_url: String, asset_manager_id:, filename: }
+        # We have asset_manager_id and filename, so we can redirect to a new style CSV preview
         # Note: using a path rather than a full URL so the user doesn't flip from a draft frontend
         #       to a live frontend if previewing a live asset of a draft document
-        "/csv-preview/#{attachment_data_id}/#{filename}"
+        "/csv-preview/#{asset_manager_id}/#{filename}"
       in { preview_url: String, url: }
-        # TODO: remove this branch once we're confident attachment_data_id is present everywhere
-        Rails.logger.warn("Attachment at #{url} using old style preview, because no attachment_data_id present")
+        # TODO: remove this branch once we're confident asset_manager_id is present everywhere
+        Rails.logger.warn("Attachment at #{url} using old style preview, because no asset_manager_id present")
         # NOTE: we're using "#{url}/preview" here because the preview_url fields have the wrong ids in them
         "#{url}/preview"
       end
