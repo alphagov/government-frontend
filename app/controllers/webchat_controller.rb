6894d25f1d3b551ab3add6bbcc288e5b1c3c7737
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-06-02 12:06:23 +0100

    WIP with minno

--- app/controllers/webchat_controller.rb
+++ app/controllers/webchat_controller.rb
@@ -7,4 +7,9 @@ class WebchatController < ApplicationController
     Rails.logger.error "Webchat error: #{e.message}"
     render plain: "Webchat configuration not found", status: :not_found
   end
+
+  def service_sign_in_options
+    return head :not_found unless content_item_path.include?("sign-in")
+
+  end
 end
