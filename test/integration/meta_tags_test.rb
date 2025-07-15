13ac3c02fe90fbe964471f54a2aba704649785cc
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-07-15 13:31:16 +0100

    Replace detailed_guide meta tags tests with publication

--- test/integration/meta_tags_test.rb
+++ test/integration/meta_tags_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 class MetaTagsTest < ActionDispatch::IntegrationTest
   test "correct meta tags are displayed for pages" do
-    case_study = GovukSchemas::RandomExample.for_schema(frontend_schema: "case_study") do |random|
+    publication = GovukSchemas::RandomExample.for_schema(frontend_schema: "publication") do |random|
       random.merge(
         "title" => "Zhe title",
         "withdrawn_notice" => {},
       )
     end
 
-    stub_content_store_has_item("/some-page", case_study.to_json)
+    stub_content_store_has_item("/some-page", publication.to_json)
 
     visit_with_cachebust "/some-page"
 
@@ -17,36 +17,12 @@ class MetaTagsTest < ActionDispatch::IntegrationTest
   end
 
   test "correct meta tags are displayed for pages without images" do
-    case_study = GovukSchemas::RandomExample.for_schema(frontend_schema: "case_study") do |random|
-      random["details"].delete("image")
-      random
-    end
+    publication = GovukSchemas::RandomExample.for_schema(frontend_schema: "publication")
 
-    stub_content_store_has_item("/some-page", case_study.to_json)
+    stub_content_store_has_item("/some-page", publication.to_json)
 
     visit_with_cachebust "/some-page"
 
     assert page.has_css?("meta[name='twitter:card'][content='summary']", visible: false)
-  end
-
-  test "correct meta tags are displayed for pages with images" do
-    case_study = GovukSchemas::RandomExample.for_schema(frontend_schema: "case_study") do |random|
-      random["details"] = random["details"].merge(
-        "image" => {
-          "url" => "https://example.org/image.jpg",
-          "alt_text" => "An accessible alt text",
-        },
-      )
-
-      random
-    end
-
-    stub_content_store_has_item("/some-page", case_study.to_json)
-
-    visit_with_cachebust "/some-page"
-
-    assert page.has_css?("meta[name='twitter:card'][content='summary_large_image']", visible: false)
-    assert page.has_css?("meta[name='twitter:image'][content='https://example.org/image.jpg']", visible: false)
-    assert page.has_css?("meta[property='og:image'][content='https://example.org/image.jpg']", visible: false)
   end
 end
