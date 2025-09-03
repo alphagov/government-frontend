9e29bbbe393a7206ca0d1e8e7807fca848f369a6
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-09-03 09:26:16 +0100

    Restyle working group header
    - apply existing styles to bottom align the translation nav (if it appears, I'm having trouble finding an example) with the heading 1

--- app/presenters/working_group_presenter.rb
+++ app/presenters/working_group_presenter.rb
@@ -22,6 +22,7 @@ class WorkingGroupPresenter < ContentItemPresenter
   def heading_and_context
     super.tap do |t|
       t[:font_size] = "xl"
+      t[:margin_bottom] = 0
       t.delete(:context)
     end
   end
