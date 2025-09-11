086524548e5843c1a367ff8e5c9a134c066c903e
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-09-11 12:41:46 +0100

    Remove detailed_guide content_ids in Single page notification
    - The exemption list contains the below content_ids and when checked in content store have the base paths for detailed_guide:
    c5c8d3cd-0dc2-4ca3-8672-8ca0a6e92165 - /guidance/dvsa-email-alerts
    70bd3a76-6606-45dd-9fb5-2b95f8667b4d - nil
    a457220c-915c-4cb1-8e41-9191fba42540 - /guidance/coronavirus-guidance-for-the-sellafield-ltd-supply-chain
    5f9c6c15-7631-11e4-a3cb-005056011aef - nil
    
    As we have moved the detailed_guide to frontend and the other 2 content_ids are nil, the exemption list is no longer relevant to have here. Also the corresponding check function would no longer be required and hence removed.
    
    The function would still be present in frontend to check for detailed_guidecontent_ids.
    https://github.com/alphagov/frontend/blob/5ebabc8d1fd547b210be65be7e2877682e961339/app/models/concerns/single_page_notification_button.rb
    
    Ref document: https://docs.publishing.service.gov.uk/manual/get-emails-about-this-page.html

--- app/presenters/content_item/single_page_notification_button.rb
+++ app/presenters/content_item/single_page_notification_button.rb
@@ -1,19 +1,7 @@
 module ContentItem
   module SinglePageNotificationButton
-    # Add the content id of the publication, detailed_guide or consultation that should be exempt from having the single page notification button
-    EXEMPTION_LIST = %w[
-      c5c8d3cd-0dc2-4ca3-8672-8ca0a6e92165
-      70bd3a76-6606-45dd-9fb5-2b95f8667b4d
-      a457220c-915c-4cb1-8e41-9191fba42540
-      5f9c6c15-7631-11e4-a3cb-005056011aef
-    ].freeze
-
-    def page_is_on_exemption_list?
-      EXEMPTION_LIST.include? content_item["content_id"]
-    end
-
     def display_single_page_notification_button?
-      !page_is_on_exemption_list? && I18n.locale == :en
+      I18n.locale == :en
     end
   end
 end
