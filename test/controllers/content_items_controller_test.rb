b719569d5b608d17609d43905493bab67102de73
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-07 14:07:03 +0100

    tests: remove suggested_orderd_related_items test
    - this test is currently just testing that the content item doesn't parse suggested_ordered_related_items into its
      ordered_related_items. Suggested ordered related items are not used, and are going to be cleaned up. There's
      no point introducing additional soon-to-be-broken examples in publishing-api just to make this test pass, so
      we just remove it.

--- test/controllers/content_items_controller_test.rb
+++ test/controllers/content_items_controller_test.rb
@@ -140,16 +140,6 @@ class ContentItemsControllerTest < ActionController::TestCase
     assert_nil content_item["links"]["ordered_related_items"]
   end
 
-  test "gets item from content store and replaces ordered_related_items there are no existing links or overrides" do
-    content_item = content_store_has_schema_example("guide", "guide-with-facet-groups")
-
-    get :show, params: { path: path_for(content_item) }
-    assert_response :success
-    assert_empty content_item["links"]["ordered_related_items"], "Content item should not have existing related links"
-    assert_not_empty content_item["links"]["suggested_ordered_related_items"], "Content item should have existing suggested related links"
-    assert_empty assigns[:content_item].content_item["links"]["ordered_related_items"]
-  end
-
   test "sets the expiry as sent by content-store" do
     content_item = content_store_has_schema_example("worldwide_organisation", "worldwide_organisation")
     stub_content_store_has_item(content_item["base_path"], content_item, max_age: 20)
