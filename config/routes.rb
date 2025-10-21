8bd23cf003f8d44228c4fa88dd0199a06f8b0a75
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-21 11:46:50 +0100

    Remove randomly generated content item support
    - It appears no-one is actually using this, and it's poorly supported/tested.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  unless Rails.env.production?
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get "random/:schema" => "randomly_generated_content_item#show"
-  end
-
   root to: "development#index"
 
   mount GovukPublishingComponents::Engine, at: "/component-guide"
