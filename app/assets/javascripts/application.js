86c7ee67acc28438947df7edc98eda656d28ef13
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-12-04 09:06:57 +0000

    Add required styles and JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,19 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/components/contents-list-with-body
-//= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/cross-service-header
+//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
-//= require govuk_publishing_components/components/image-card
-//= require govuk_publishing_components/components/intervention
+//= require govuk_publishing_components/components/layout-super-navigation-header
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/print-link
-//= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/service-navigation
 //= require govuk_publishing_components/components/single-page-notification-button
+//= require govuk_publishing_components/components/skip-link
 //= require govuk_publishing_components/components/step-by-step-nav
 
 //= require govuk_web_banners/dependencies
