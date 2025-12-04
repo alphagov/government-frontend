587e78b6c78a062ba380ca33f8726b0ccb1ddab3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-12-04 09:31:57 +0000

    Remove app references to slimmer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Slimmer::Template
-  slimmer_template "gem_layout"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
